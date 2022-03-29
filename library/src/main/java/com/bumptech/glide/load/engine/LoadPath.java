97cdc090c12a2c610bda8e0cb694bed8e2755276
nimakarimipour <karimipour.nima@gmail.com>
2022-03-29 12:21:00 -0700

    Final Result

--- library/src/main/java/com/bumptech/glide/load/engine/LoadPath.java
+++ library/src/main/java/com/bumptech/glide/load/engine/LoadPath.java
@@ -1,5 +1,6 @@
 package com.bumptech.glide.load.engine;
 
+import androidx.annotation.Nullable;
 import androidx.annotation.NonNull;
 import androidx.core.util.Pools.Pool;
 import com.bumptech.glide.load.Options;
@@ -21,80 +22,58 @@ import java.util.List;
  *     of the decode paths succeeds.
  */
 public class LoadPath<Data, ResourceType, Transcode> {
-  private final Class<Data> dataClass;
-  private final Pool<List<Throwable>> listPool;
-  private final List<? extends DecodePath<Data, ResourceType, Transcode>> decodePaths;
-  private final String failureMessage;
 
-  public LoadPath(
-      Class<Data> dataClass,
-      Class<ResourceType> resourceClass,
-      Class<Transcode> transcodeClass,
-      List<DecodePath<Data, ResourceType, Transcode>> decodePaths,
-      Pool<List<Throwable>> listPool) {
-    this.dataClass = dataClass;
-    this.listPool = listPool;
-    this.decodePaths = Preconditions.checkNotEmpty(decodePaths);
-    failureMessage =
-        "Failed LoadPath{"
-            + dataClass.getSimpleName()
-            + "->"
-            + resourceClass.getSimpleName()
-            + "->"
-            + transcodeClass.getSimpleName()
-            + "}";
-  }
+    private final Class<Data> dataClass;
 
-  public Resource<Transcode> load(
-      DataRewinder<Data> rewinder,
-      @NonNull Options options,
-      int width,
-      int height,
-      DecodePath.DecodeCallback<ResourceType> decodeCallback)
-      throws GlideException {
-    List<Throwable> throwables = Preconditions.checkNotNull(listPool.acquire());
-    try {
-      return loadWithExceptionList(rewinder, options, width, height, decodeCallback, throwables);
-    } finally {
-      listPool.release(throwables);
-    }
-  }
+    @Nullable
+    private final Pool<List<Throwable>> listPool;
 
-  private Resource<Transcode> loadWithExceptionList(
-      DataRewinder<Data> rewinder,
-      @NonNull Options options,
-      int width,
-      int height,
-      DecodePath.DecodeCallback<ResourceType> decodeCallback,
-      List<Throwable> exceptions)
-      throws GlideException {
-    Resource<Transcode> result = null;
-    //noinspection ForLoopReplaceableByForEach to improve perf
-    for (int i = 0, size = decodePaths.size(); i < size; i++) {
-      DecodePath<Data, ResourceType, Transcode> path = decodePaths.get(i);
-      try {
-        result = path.decode(rewinder, width, height, options, decodeCallback);
-      } catch (GlideException e) {
-        exceptions.add(e);
-      }
-      if (result != null) {
-        break;
-      }
+    private final List<? extends DecodePath<Data, ResourceType, Transcode>> decodePaths;
+
+    private final String failureMessage;
+
+    public LoadPath(Class<Data> dataClass, Class<ResourceType> resourceClass, Class<Transcode> transcodeClass, List<DecodePath<Data, ResourceType, Transcode>> decodePaths, @Nullable Pool<List<Throwable>> listPool) {
+        this.dataClass = dataClass;
+        this.listPool = listPool;
+        this.decodePaths = Preconditions.checkNotEmpty(decodePaths);
+        failureMessage = "Failed LoadPath{" + dataClass.getSimpleName() + "->" + resourceClass.getSimpleName() + "->" + transcodeClass.getSimpleName() + "}";
     }
 
-    if (result == null) {
-      throw new GlideException(failureMessage, new ArrayList<>(exceptions));
+    public Resource<Transcode> load(DataRewinder<Data> rewinder, @NonNull Options options, int width, int height, DecodePath.DecodeCallback<ResourceType> decodeCallback) throws GlideException {
+        List<Throwable> throwables = Preconditions.checkNotNull(listPool.acquire());
+        try {
+            return loadWithExceptionList(rewinder, options, width, height, decodeCallback, throwables);
+        } finally {
+            listPool.release(throwables);
+        }
     }
 
-    return result;
-  }
+    private Resource<Transcode> loadWithExceptionList(DataRewinder<Data> rewinder, @NonNull Options options, int width, int height, DecodePath.DecodeCallback<ResourceType> decodeCallback, List<Throwable> exceptions) throws GlideException {
+        Resource<Transcode> result = null;
+        // noinspection ForLoopReplaceableByForEach to improve perf
+        for (int i = 0, size = decodePaths.size(); i < size; i++) {
+            DecodePath<Data, ResourceType, Transcode> path = decodePaths.get(i);
+            try {
+                result = path.decode(rewinder, width, height, options, decodeCallback);
+            } catch (GlideException e) {
+                exceptions.add(e);
+            }
+            if (result != null) {
+                break;
+            }
+        }
+        if (result == null) {
+            throw new GlideException(failureMessage, new ArrayList<>(exceptions));
+        }
+        return result;
+    }
 
-  public Class<Data> getDataClass() {
-    return dataClass;
-  }
+    public Class<Data> getDataClass() {
+        return dataClass;
+    }
 
-  @Override
-  public String toString() {
-    return "LoadPath{" + "decodePaths=" + Arrays.toString(decodePaths.toArray()) + '}';
-  }
+    @Override
+    public String toString() {
+        return "LoadPath{" + "decodePaths=" + Arrays.toString(decodePaths.toArray()) + '}';
+    }
 }
--- library/src/main/java/com/bumptech/glide/load/engine/cache/DiskLruCacheFactory.java
+++ library/src/main/java/com/bumptech/glide/load/engine/cache/DiskLruCacheFactory.java
@@ -1,5 +1,6 @@
 package com.bumptech.glide.load.engine.cache;
 
+import androidx.annotation.Nullable;
 import java.io.File;
 
 /**
@@ -12,63 +13,63 @@ import java.io.File;
 // Public API.
 @SuppressWarnings("unused")
 public class DiskLruCacheFactory implements DiskCache.Factory {
-  private final long diskCacheSize;
-  private final CacheDirectoryGetter cacheDirectoryGetter;
 
-  /** Interface called out of UI thread to get the cache folder. */
-  public interface CacheDirectoryGetter {
-    File getCacheDirectory();
-  }
+    private final long diskCacheSize;
 
-  public DiskLruCacheFactory(final String diskCacheFolder, long diskCacheSize) {
-    this(
-        new CacheDirectoryGetter() {
-          @Override
-          public File getCacheDirectory() {
-            return new File(diskCacheFolder);
-          }
-        },
-        diskCacheSize);
-  }
+    private final CacheDirectoryGetter cacheDirectoryGetter;
 
-  public DiskLruCacheFactory(
-      final String diskCacheFolder, final String diskCacheName, long diskCacheSize) {
-    this(
-        new CacheDirectoryGetter() {
-          @Override
-          public File getCacheDirectory() {
-            return new File(diskCacheFolder, diskCacheName);
-          }
-        },
-        diskCacheSize);
-  }
+    /**
+     * Interface called out of UI thread to get the cache folder.
+     */
+    public interface CacheDirectoryGetter {
 
-  /**
-   * When using this constructor {@link CacheDirectoryGetter#getCacheDirectory()} will be called out
-   * of UI thread, allowing to do I/O access without performance impacts.
-   *
-   * @param cacheDirectoryGetter Interface called out of UI thread to get the cache folder.
-   * @param diskCacheSize Desired max bytes size for the LRU disk cache.
-   */
-  // Public API.
-  @SuppressWarnings("WeakerAccess")
-  public DiskLruCacheFactory(CacheDirectoryGetter cacheDirectoryGetter, long diskCacheSize) {
-    this.diskCacheSize = diskCacheSize;
-    this.cacheDirectoryGetter = cacheDirectoryGetter;
-  }
-
-  @Override
-  public DiskCache build() {
-    File cacheDir = cacheDirectoryGetter.getCacheDirectory();
-
-    if (cacheDir == null) {
-      return null;
+        File getCacheDirectory();
     }
 
-    if (cacheDir.isDirectory() || cacheDir.mkdirs()) {
-      return DiskLruCacheWrapper.create(cacheDir, diskCacheSize);
+    public DiskLruCacheFactory(final String diskCacheFolder, long diskCacheSize) {
+        this(new CacheDirectoryGetter() {
+
+            @Override
+            public File getCacheDirectory() {
+                return new File(diskCacheFolder);
+            }
+        }, diskCacheSize);
     }
 
-    return null;
-  }
+    public DiskLruCacheFactory(final String diskCacheFolder, final String diskCacheName, long diskCacheSize) {
+        this(new CacheDirectoryGetter() {
+
+            @Override
+            public File getCacheDirectory() {
+                return new File(diskCacheFolder, diskCacheName);
+            }
+        }, diskCacheSize);
+    }
+
+    /**
+     * When using this constructor {@link CacheDirectoryGetter#getCacheDirectory()} will be called out
+     * of UI thread, allowing to do I/O access without performance impacts.
+     *
+     * @param cacheDirectoryGetter Interface called out of UI thread to get the cache folder.
+     * @param diskCacheSize Desired max bytes size for the LRU disk cache.
+     */
+    // Public API.
+    @SuppressWarnings("WeakerAccess")
+    public DiskLruCacheFactory(CacheDirectoryGetter cacheDirectoryGetter, long diskCacheSize) {
+        this.diskCacheSize = diskCacheSize;
+        this.cacheDirectoryGetter = cacheDirectoryGetter;
+    }
+
+    @Override
+    @Nullable
+    public DiskCache build() {
+        File cacheDir = cacheDirectoryGetter.getCacheDirectory();
+        if (cacheDir == null) {
+            return null;
+        }
+        if (cacheDir.isDirectory() || cacheDir.mkdirs()) {
+            return DiskLruCacheWrapper.create(cacheDir, diskCacheSize);
+        }
+        return null;
+    }
 }
