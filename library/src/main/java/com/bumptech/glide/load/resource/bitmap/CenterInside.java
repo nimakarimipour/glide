2c9d206a925b9c54d76ccf755b7d0c70522e530f
nimakarimipour <karimipour.nima@gmail.com>
2022-04-07 07:29:54 -0700

    Final Result

--- library/src/main/java/com/bumptech/glide/load/resource/bitmap/CenterInside.java
+++ library/src/main/java/com/bumptech/glide/load/resource/bitmap/CenterInside.java
@@ -1,5 +1,6 @@
 package com.bumptech.glide.load.resource.bitmap;
 
+import androidx.annotation.Nullable;
 import android.graphics.Bitmap;
 import androidx.annotation.NonNull;
 import com.bumptech.glide.load.engine.bitmap_recycle.BitmapPool;
@@ -13,27 +14,28 @@ import java.security.MessageDigest;
  * image's aspect ratio).
  */
 public class CenterInside extends BitmapTransformation {
-  private static final String ID = "com.bumptech.glide.load.resource.bitmap.CenterInside";
-  private static final byte[] ID_BYTES = ID.getBytes(CHARSET);
 
-  @Override
-  protected Bitmap transform(
-      @NonNull BitmapPool pool, @NonNull Bitmap toTransform, int outWidth, int outHeight) {
-    return TransformationUtils.centerInside(pool, toTransform, outWidth, outHeight);
-  }
+    private static final String ID = "com.bumptech.glide.load.resource.bitmap.CenterInside";
 
-  @Override
-  public boolean equals(Object o) {
-    return o instanceof CenterInside;
-  }
+    private static final byte[] ID_BYTES = ID.getBytes(CHARSET);
 
-  @Override
-  public int hashCode() {
-    return ID.hashCode();
-  }
+    @Override
+    protected Bitmap transform(@NonNull BitmapPool pool, @NonNull Bitmap toTransform, int outWidth, int outHeight) {
+        return TransformationUtils.centerInside(pool, toTransform, outWidth, outHeight);
+    }
 
-  @Override
-  public void updateDiskCacheKey(@NonNull MessageDigest messageDigest) {
-    messageDigest.update(ID_BYTES);
-  }
+    @Override
+    public boolean equals(Object o) {
+        return o instanceof CenterInside;
+    }
+
+    @Override
+    public int hashCode() {
+        return ID.hashCode();
+    }
+
+    @Override
+    public void updateDiskCacheKey(@NonNull MessageDigest messageDigest) {
+        messageDigest.update(ID_BYTES);
+    }
 }
--- library/src/main/java/com/bumptech/glide/request/target/BitmapImageViewTarget.java
+++ library/src/main/java/com/bumptech/glide/request/target/BitmapImageViewTarget.java
@@ -1,5 +1,6 @@
 package com.bumptech.glide.request.target;
 
+import androidx.annotation.Nullable;
 import android.graphics.Bitmap;
 import android.widget.ImageView;
 
@@ -8,30 +9,31 @@ import android.widget.ImageView;
  * android.graphics.Bitmap} in an {@link android.widget.ImageView}.
  */
 public class BitmapImageViewTarget extends ImageViewTarget<Bitmap> {
-  // Public API.
-  @SuppressWarnings("WeakerAccess")
-  public BitmapImageViewTarget(ImageView view) {
-    super(view);
-  }
 
-  /**
-   * @deprecated Use {@link #waitForLayout()} instead.
-   */
-  // Public API.
-  @SuppressWarnings({"unused", "deprecation"})
-  @Deprecated
-  public BitmapImageViewTarget(ImageView view, boolean waitForLayout) {
-    super(view, waitForLayout);
-  }
+    // Public API.
+    @SuppressWarnings("WeakerAccess")
+    public BitmapImageViewTarget(ImageView view) {
+        super(view);
+    }
 
-  /**
-   * Sets the {@link android.graphics.Bitmap} on the view using {@link
-   * android.widget.ImageView#setImageBitmap(android.graphics.Bitmap)}.
-   *
-   * @param resource The bitmap to display.
-   */
-  @Override
-  protected void setResource(Bitmap resource) {
-    view.setImageBitmap(resource);
-  }
+    /**
+     * @deprecated Use {@link #waitForLayout()} instead.
+     */
+    // Public API.
+    @SuppressWarnings({ "unused", "deprecation" })
+    @Deprecated
+    public BitmapImageViewTarget(ImageView view, boolean waitForLayout) {
+        super(view, waitForLayout);
+    }
+
+    /**
+     * Sets the {@link android.graphics.Bitmap} on the view using {@link
+     * android.widget.ImageView#setImageBitmap(android.graphics.Bitmap)}.
+     *
+     * @param resource The bitmap to display.
+     */
+    @Override
+    protected void setResource(@Nullable Bitmap resource) {
+        view.setImageBitmap(resource);
+    }
 }
