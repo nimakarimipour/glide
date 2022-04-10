c0f77773cc1bbadcbcc3e2e49f6fc05851288a05
nimakarimipour <karimipour.nima@gmail.com>
2022-04-10 11:39:59 -0700

    Final Result

--- library/src/main/java/com/bumptech/glide/load/engine/bitmap_recycle/IntegerArrayAdapter.java
+++ library/src/main/java/com/bumptech/glide/load/engine/bitmap_recycle/IntegerArrayAdapter.java
@@ -1,27 +1,32 @@
 package com.bumptech.glide.load.engine.bitmap_recycle;
 
-/** Adapter for handling primitive int arrays. */
+import androidx.annotation.Nullable;
+
+/**
+ * Adapter for handling primitive int arrays.
+ */
 @SuppressWarnings("PMD.UseVarargs")
 public final class IntegerArrayAdapter implements ArrayAdapterInterface<int[]> {
-  private static final String TAG = "IntegerArrayPool";
 
-  @Override
-  public String getTag() {
-    return TAG;
-  }
+    private static final String TAG = "IntegerArrayPool";
 
-  @Override
-  public int getArrayLength(int[] array) {
-    return array.length;
-  }
+    @Override
+    public String getTag() {
+        return TAG;
+    }
 
-  @Override
-  public int[] newArray(int length) {
-    return new int[length];
-  }
+    @Override
+    public int getArrayLength(@Nullable int[] array) {
+        return array.length;
+    }
 
-  @Override
-  public int getElementSizeInBytes() {
-    return 4;
-  }
+    @Override
+    public int[] newArray(int length) {
+        return new int[length];
+    }
+
+    @Override
+    public int getElementSizeInBytes() {
+        return 4;
+    }
 }
--- library/src/main/java/com/bumptech/glide/util/ViewPreloadSizeProvider.java
+++ library/src/main/java/com/bumptech/glide/util/ViewPreloadSizeProvider.java
@@ -16,83 +16,89 @@ import java.util.Arrays;
  *
  * @param <T> The type of the model the size should be provided for.
  */
-public class ViewPreloadSizeProvider<T>
-    implements ListPreloader.PreloadSizeProvider<T>, SizeReadyCallback {
-  private int[] size;
-  // We need to keep a strong reference to the Target so that it isn't garbage collected due to a
-  // weak reference
-  // while we're waiting to get its size.
-  @SuppressWarnings("unused")
-  private SizeViewTarget viewTarget;
+public class ViewPreloadSizeProvider<T> implements ListPreloader.PreloadSizeProvider<T>, SizeReadyCallback {
 
-  /**
-   * Constructor that does nothing by default and requires users to call {@link
-   * #setView(android.view.View)} when a View is available to registerComponents the dimensions
-   * returned by this class.
-   */
-  public ViewPreloadSizeProvider() {
-    // This constructor is intentionally empty. Nothing special is needed here.
-  }
+    @Nullable
+    private int[] size;
 
-  /**
-   * Constructor that will extract the preload size from a given {@link android.view.View}.
-   *
-   * @param view A not null View the size will be extracted from async using an {@link
-   *     android.view.ViewTreeObserver .OnPreDrawListener}
-   */
-  // Public API.
-  @SuppressWarnings("WeakerAccess")
-  public ViewPreloadSizeProvider(@NonNull View view) {
-    viewTarget = new SizeViewTarget(view);
-    viewTarget.getSize(this);
-  }
+    // We need to keep a strong reference to the Target so that it isn't garbage collected due to a
+    // weak reference
+    // while we're waiting to get its size.
+    @SuppressWarnings("unused")
+    @Nullable
+    private SizeViewTarget viewTarget;
 
-  @Nullable
-  @Override
-  public int[] getPreloadSize(@NonNull T item, int adapterPosition, int itemPosition) {
-    if (size == null) {
-      return null;
-    } else {
-      return Arrays.copyOf(size, size.length);
+    /**
+     * Constructor that does nothing by default and requires users to call {@link
+     * #setView(android.view.View)} when a View is available to registerComponents the dimensions
+     * returned by this class.
+     */
+    public ViewPreloadSizeProvider() {
+        // This constructor is intentionally empty. Nothing special is needed here.
     }
-  }
 
-  @Override
-  public void onSizeReady(int width, int height) {
-    size = new int[] {width, height};
-    viewTarget = null;
-  }
-
-  /**
-   * Sets the {@link android.view.View} the size will be extracted.
-   *
-   * <p>Note - only the first call to this method will be obeyed, subsequent requests will be
-   * ignored.
-   *
-   * @param view A not null View the size will be extracted async with an {@link
-   *     android.view.ViewTreeObserver .OnPreDrawListener}
-   */
-  public void setView(@NonNull View view) {
-    if (size != null || viewTarget != null) {
-      return;
+    /**
+     * Constructor that will extract the preload size from a given {@link android.view.View}.
+     *
+     * @param view A not null View the size will be extracted from async using an {@link
+     *     android.view.ViewTreeObserver .OnPreDrawListener}
+     */
+    // Public API.
+    @SuppressWarnings("WeakerAccess")
+    public ViewPreloadSizeProvider(@NonNull View view) {
+        viewTarget = new SizeViewTarget(view);
+        viewTarget.getSize(this);
     }
-    viewTarget = new SizeViewTarget(view);
-    viewTarget.getSize(this);
-  }
 
-  static final class SizeViewTarget extends CustomViewTarget<View, Object> {
-    SizeViewTarget(@NonNull View view) {
-      super(view);
+    @Nullable
+    @Override
+    public int[] getPreloadSize(@NonNull T item, int adapterPosition, int itemPosition) {
+        if (size == null) {
+            return null;
+        } else {
+            return Arrays.copyOf(size, size.length);
+        }
     }
 
     @Override
-    protected void onResourceCleared(@Nullable Drawable placeholder) {}
+    public void onSizeReady(int width, int height) {
+        size = new int[] { width, height };
+        viewTarget = null;
+    }
 
-    @Override
-    public void onLoadFailed(@Nullable Drawable errorDrawable) {}
+    /**
+     * Sets the {@link android.view.View} the size will be extracted.
+     *
+     * <p>Note - only the first call to this method will be obeyed, subsequent requests will be
+     * ignored.
+     *
+     * @param view A not null View the size will be extracted async with an {@link
+     *     android.view.ViewTreeObserver .OnPreDrawListener}
+     */
+    public void setView(@NonNull View view) {
+        if (size != null || viewTarget != null) {
+            return;
+        }
+        viewTarget = new SizeViewTarget(view);
+        viewTarget.getSize(this);
+    }
 
-    @Override
-    public void onResourceReady(
-        @NonNull Object resource, @Nullable Transition<? super Object> transition) {}
-  }
+    static final class SizeViewTarget extends CustomViewTarget<View, Object> {
+
+        SizeViewTarget(@NonNull View view) {
+            super(view);
+        }
+
+        @Override
+        protected void onResourceCleared(@Nullable Drawable placeholder) {
+        }
+
+        @Override
+        public void onLoadFailed(@Nullable Drawable errorDrawable) {
+        }
+
+        @Override
+        public void onResourceReady(@NonNull Object resource, @Nullable Transition<? super Object> transition) {
+        }
+    }
 }
