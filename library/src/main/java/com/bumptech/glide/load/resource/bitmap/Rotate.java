b189a3e2f1193c04ddf4e625103cd56f9379b7ea
nimakarimipour <karimipour.nima@gmail.com>
2022-04-10 12:25:36 -0700

    Final Result

--- library/src/main/java/com/bumptech/glide/load/resource/bitmap/Rotate.java
+++ library/src/main/java/com/bumptech/glide/load/resource/bitmap/Rotate.java
@@ -1,5 +1,6 @@
 package com.bumptech.glide.load.resource.bitmap;
 
+import androidx.annotation.Nullable;
 import android.graphics.Bitmap;
 import androidx.annotation.NonNull;
 import com.bumptech.glide.load.engine.bitmap_recycle.BitmapPool;
@@ -7,46 +8,48 @@ import com.bumptech.glide.util.Util;
 import java.nio.ByteBuffer;
 import java.security.MessageDigest;
 
-/** A {@link BitmapTransformation} which rotates the bitmap. */
+/**
+ * A {@link BitmapTransformation} which rotates the bitmap.
+ */
 public class Rotate extends BitmapTransformation {
-  private static final String ID = "com.bumptech.glide.load.resource.bitmap.Rotate";
-  private static final byte[] ID_BYTES = ID.getBytes(CHARSET);
 
-  private final int degreesToRotate;
+    private static final String ID = "com.bumptech.glide.load.resource.bitmap.Rotate";
 
-  /**
-   * @param degreesToRotate number of degrees to rotate the image by. If zero the original image is
-   *     not modified.
-   */
-  public Rotate(int degreesToRotate) {
-    this.degreesToRotate = degreesToRotate;
-  }
+    private static final byte[] ID_BYTES = ID.getBytes(CHARSET);
 
-  @Override
-  protected Bitmap transform(
-      @NonNull BitmapPool pool, @NonNull Bitmap toTransform, int outWidth, int outHeight) {
-    return TransformationUtils.rotateImage(toTransform, degreesToRotate);
-  }
+    private final int degreesToRotate;
 
-  @Override
-  public boolean equals(Object o) {
-    if (o instanceof Rotate) {
-      Rotate other = (Rotate) o;
-      return degreesToRotate == other.degreesToRotate;
+    /**
+     * @param degreesToRotate number of degrees to rotate the image by. If zero the original image is
+     *     not modified.
+     */
+    public Rotate(int degreesToRotate) {
+        this.degreesToRotate = degreesToRotate;
     }
-    return false;
-  }
 
-  @Override
-  public int hashCode() {
-    return Util.hashCode(ID.hashCode(), Util.hashCode(degreesToRotate));
-  }
+    @Override
+    protected Bitmap transform(@NonNull BitmapPool pool, @NonNull Bitmap toTransform, int outWidth, int outHeight) {
+        return TransformationUtils.rotateImage(toTransform, degreesToRotate);
+    }
 
-  @Override
-  public void updateDiskCacheKey(@NonNull MessageDigest messageDigest) {
-    messageDigest.update(ID_BYTES);
+    @Override
+    public boolean equals(Object o) {
+        if (o instanceof Rotate) {
+            Rotate other = (Rotate) o;
+            return degreesToRotate == other.degreesToRotate;
+        }
+        return false;
+    }
 
-    byte[] degreesData = ByteBuffer.allocate(4).putInt(degreesToRotate).array();
-    messageDigest.update(degreesData);
-  }
+    @Override
+    public int hashCode() {
+        return Util.hashCode(ID.hashCode(), Util.hashCode(degreesToRotate));
+    }
+
+    @Override
+    public void updateDiskCacheKey(@NonNull MessageDigest messageDigest) {
+        messageDigest.update(ID_BYTES);
+        byte[] degreesData = ByteBuffer.allocate(4).putInt(degreesToRotate).array();
+        messageDigest.update(degreesData);
+    }
 }
