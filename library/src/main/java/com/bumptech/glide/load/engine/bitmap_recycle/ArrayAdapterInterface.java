0159feff9d34be792be40286f497e691d2404370
nimakarimipour <karimipour.nima@gmail.com>
2022-04-10 10:52:29 -0700

    Final Result

--- library/src/main/java/com/bumptech/glide/load/engine/bitmap_recycle/ArrayAdapterInterface.java
+++ library/src/main/java/com/bumptech/glide/load/engine/bitmap_recycle/ArrayAdapterInterface.java
@@ -1,4 +1,7 @@
 package com.bumptech.glide.load.engine.bitmap_recycle;
+
+import androidx.annotation.Nullable;
+
 /**
  * Interface for handling operations on a primitive array type.
  *
@@ -6,15 +9,23 @@ package com.bumptech.glide.load.engine.bitmap_recycle;
  */
 interface ArrayAdapterInterface<T> {
 
-  /** TAG for logging. */
-  String getTag();
+    /**
+     * TAG for logging.
+     */
+    String getTag();
 
-  /** Return the length of the given array. */
-  int getArrayLength(T array);
+    /**
+     * Return the length of the given array.
+     */
+    int getArrayLength(@Nullable T array);
 
-  /** Allocate and return an array of the specified size. */
-  T newArray(int length);
+    /**
+     * Allocate and return an array of the specified size.
+     */
+    T newArray(int length);
 
-  /** Return the size of an element in the array in bytes (e.g. for int return 4). */
-  int getElementSizeInBytes();
+    /**
+     * Return the size of an element in the array in bytes (e.g. for int return 4).
+     */
+    int getElementSizeInBytes();
 }
--- library/src/main/java/com/bumptech/glide/load/engine/bitmap_recycle/ByteArrayAdapter.java
+++ library/src/main/java/com/bumptech/glide/load/engine/bitmap_recycle/ByteArrayAdapter.java
@@ -1,27 +1,32 @@
 package com.bumptech.glide.load.engine.bitmap_recycle;
 
-/** Adapter for handling primitive byte arrays. */
+import androidx.annotation.Nullable;
+
+/**
+ * Adapter for handling primitive byte arrays.
+ */
 @SuppressWarnings("PMD.UseVarargs")
 public final class ByteArrayAdapter implements ArrayAdapterInterface<byte[]> {
-  private static final String TAG = "ByteArrayPool";
 
-  @Override
-  public String getTag() {
-    return TAG;
-  }
+    private static final String TAG = "ByteArrayPool";
 
-  @Override
-  public int getArrayLength(byte[] array) {
-    return array.length;
-  }
+    @Override
+    public String getTag() {
+        return TAG;
+    }
 
-  @Override
-  public byte[] newArray(int length) {
-    return new byte[length];
-  }
+    @Override
+    public int getArrayLength(@Nullable byte[] array) {
+        return array.length;
+    }
 
-  @Override
-  public int getElementSizeInBytes() {
-    return 1;
-  }
+    @Override
+    public byte[] newArray(int length) {
+        return new byte[length];
+    }
+
+    @Override
+    public int getElementSizeInBytes() {
+        return 1;
+    }
 }
