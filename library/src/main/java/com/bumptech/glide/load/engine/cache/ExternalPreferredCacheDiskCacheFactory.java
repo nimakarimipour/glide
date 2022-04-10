74a345c2883d7153444b72be54324dd1e6d6c20b
nimakarimipour <karimipour.nima@gmail.com>
2022-04-10 11:51:04 -0700

    Final Result

--- library/src/main/java/com/bumptech/glide/load/engine/cache/ExternalPreferredCacheDiskCacheFactory.java
+++ library/src/main/java/com/bumptech/glide/load/engine/cache/ExternalPreferredCacheDiskCacheFactory.java
@@ -12,58 +12,50 @@ import java.io.File;
  * <p><b>Images can be read by everyone when using external disk cache.</b>
  */
 // Public API.
-@SuppressWarnings({"unused", "WeakerAccess"})
+@SuppressWarnings({ "unused", "WeakerAccess" })
 public final class ExternalPreferredCacheDiskCacheFactory extends DiskLruCacheFactory {
 
-  public ExternalPreferredCacheDiskCacheFactory(Context context) {
-    this(
-        context,
-        DiskCache.Factory.DEFAULT_DISK_CACHE_DIR,
-        DiskCache.Factory.DEFAULT_DISK_CACHE_SIZE);
-  }
+    public ExternalPreferredCacheDiskCacheFactory(Context context) {
+        this(context, DiskCache.Factory.DEFAULT_DISK_CACHE_DIR, DiskCache.Factory.DEFAULT_DISK_CACHE_SIZE);
+    }
 
-  public ExternalPreferredCacheDiskCacheFactory(Context context, long diskCacheSize) {
-    this(context, DiskCache.Factory.DEFAULT_DISK_CACHE_DIR, diskCacheSize);
-  }
+    public ExternalPreferredCacheDiskCacheFactory(Context context, long diskCacheSize) {
+        this(context, DiskCache.Factory.DEFAULT_DISK_CACHE_DIR, diskCacheSize);
+    }
 
-  public ExternalPreferredCacheDiskCacheFactory(
-      final Context context, final String diskCacheName, final long diskCacheSize) {
-    super(
-        new CacheDirectoryGetter() {
-          @Nullable
-          private File getInternalCacheDirectory() {
-            File cacheDirectory = context.getCacheDir();
-            if (cacheDirectory == null) {
-              return null;
-            }
-            if (diskCacheName != null) {
-              return new File(cacheDirectory, diskCacheName);
-            }
-            return cacheDirectory;
-          }
+    public ExternalPreferredCacheDiskCacheFactory(final Context context, final String diskCacheName, final long diskCacheSize) {
+        super(new CacheDirectoryGetter() {
 
-          @Override
-          public File getCacheDirectory() {
-            File internalCacheDirectory = getInternalCacheDirectory();
-
-            // Already used internal cache, so keep using that one,
-            // thus avoiding using both external and internal with transient errors.
-            if ((null != internalCacheDirectory) && internalCacheDirectory.exists()) {
-              return internalCacheDirectory;
+            @Nullable
+            private File getInternalCacheDirectory() {
+                File cacheDirectory = context.getCacheDir();
+                if (cacheDirectory == null) {
+                    return null;
+                }
+                if (diskCacheName != null) {
+                    return new File(cacheDirectory, diskCacheName);
+                }
+                return cacheDirectory;
             }
 
-            File cacheDirectory = context.getExternalCacheDir();
-
-            // Shared storage is not available.
-            if ((cacheDirectory == null) || (!cacheDirectory.canWrite())) {
-              return internalCacheDirectory;
+            @Override
+            public File getCacheDirectory() {
+                File internalCacheDirectory = getInternalCacheDirectory();
+                // Already used internal cache, so keep using that one,
+                // thus avoiding using both external and internal with transient errors.
+                if ((null != internalCacheDirectory) && internalCacheDirectory.exists()) {
+                    return internalCacheDirectory;
+                }
+                File cacheDirectory = context.getExternalCacheDir();
+                // Shared storage is not available.
+                if ((cacheDirectory == null) || (!cacheDirectory.canWrite())) {
+                    return internalCacheDirectory;
+                }
+                if (diskCacheName != null) {
+                    return new File(cacheDirectory, diskCacheName);
+                }
+                return cacheDirectory;
             }
-            if (diskCacheName != null) {
-              return new File(cacheDirectory, diskCacheName);
-            }
-            return cacheDirectory;
-          }
-        },
-        diskCacheSize);
-  }
+        }, diskCacheSize);
+    }
 }
