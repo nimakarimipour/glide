cb88d866367d7ada252aa446b785c2687549795f
nimakarimipour <karimipour.nima@gmail.com>
2022-04-10 11:08:48 -0700

    Final Result

--- library/src/main/java/com/bumptech/glide/load/engine/cache/InternalCacheDiskCacheFactory.java
+++ library/src/main/java/com/bumptech/glide/load/engine/cache/InternalCacheDiskCacheFactory.java
@@ -8,36 +8,31 @@ import java.io.File;
  * disk cache directory.
  */
 // Public API.
-@SuppressWarnings({"WeakerAccess", "unused"})
+@SuppressWarnings({ "WeakerAccess", "unused" })
 public final class InternalCacheDiskCacheFactory extends DiskLruCacheFactory {
 
-  public InternalCacheDiskCacheFactory(Context context) {
-    this(
-        context,
-        DiskCache.Factory.DEFAULT_DISK_CACHE_DIR,
-        DiskCache.Factory.DEFAULT_DISK_CACHE_SIZE);
-  }
+    public InternalCacheDiskCacheFactory(Context context) {
+        this(context, DiskCache.Factory.DEFAULT_DISK_CACHE_DIR, DiskCache.Factory.DEFAULT_DISK_CACHE_SIZE);
+    }
 
-  public InternalCacheDiskCacheFactory(Context context, long diskCacheSize) {
-    this(context, DiskCache.Factory.DEFAULT_DISK_CACHE_DIR, diskCacheSize);
-  }
+    public InternalCacheDiskCacheFactory(Context context, long diskCacheSize) {
+        this(context, DiskCache.Factory.DEFAULT_DISK_CACHE_DIR, diskCacheSize);
+    }
 
-  public InternalCacheDiskCacheFactory(
-      final Context context, final String diskCacheName, long diskCacheSize) {
-    super(
-        new CacheDirectoryGetter() {
-          @Override
-          public File getCacheDirectory() {
-            File cacheDirectory = context.getCacheDir();
-            if (cacheDirectory == null) {
-              return null;
+    public InternalCacheDiskCacheFactory(final Context context, final String diskCacheName, long diskCacheSize) {
+        super(new CacheDirectoryGetter() {
+
+            @Override
+            public File getCacheDirectory() {
+                File cacheDirectory = context.getCacheDir();
+                if (cacheDirectory == null) {
+                    return null;
+                }
+                if (diskCacheName != null) {
+                    return new File(cacheDirectory, diskCacheName);
+                }
+                return cacheDirectory;
             }
-            if (diskCacheName != null) {
-              return new File(cacheDirectory, diskCacheName);
-            }
-            return cacheDirectory;
-          }
-        },
-        diskCacheSize);
-  }
+        }, diskCacheSize);
+    }
 }
--- library/src/main/java/com/bumptech/glide/request/transition/ViewPropertyAnimationFactory.java
+++ library/src/main/java/com/bumptech/glide/request/transition/ViewPropertyAnimationFactory.java
@@ -1,5 +1,6 @@
 package com.bumptech.glide.request.transition;
 
+import androidx.annotation.Nullable;
 import com.bumptech.glide.load.DataSource;
 
 /**
@@ -8,28 +9,30 @@ import com.bumptech.glide.load.DataSource;
  * @param <R> The type of the resource that will be transitioned into a view.
  */
 public class ViewPropertyAnimationFactory<R> implements TransitionFactory<R> {
-  private final ViewPropertyTransition.Animator animator;
-  private ViewPropertyTransition<R> animation;
 
-  public ViewPropertyAnimationFactory(ViewPropertyTransition.Animator animator) {
-    this.animator = animator;
-  }
+    private final ViewPropertyTransition.Animator animator;
 
-  /**
-   * Returns a new {@link Transition} for the given arguments. If isMemoryCache is {@code true} or
-   * isFirstImage is {@code false}, returns a {@link NoTransition} and otherwise returns a new
-   * {@link ViewPropertyTransition} for the {@link ViewPropertyTransition.Animator} provided in the
-   * constructor.
-   */
-  @Override
-  public Transition<R> build(DataSource dataSource, boolean isFirstResource) {
-    if (dataSource == DataSource.MEMORY_CACHE || !isFirstResource) {
-      return NoTransition.get();
-    }
-    if (animation == null) {
-      animation = new ViewPropertyTransition<>(animator);
+    @Nullable
+    private ViewPropertyTransition<R> animation;
+
+    public ViewPropertyAnimationFactory(ViewPropertyTransition.Animator animator) {
+        this.animator = animator;
     }
 
-    return animation;
-  }
+    /**
+     * Returns a new {@link Transition} for the given arguments. If isMemoryCache is {@code true} or
+     * isFirstImage is {@code false}, returns a {@link NoTransition} and otherwise returns a new
+     * {@link ViewPropertyTransition} for the {@link ViewPropertyTransition.Animator} provided in the
+     * constructor.
+     */
+    @Override
+    public Transition<R> build(DataSource dataSource, boolean isFirstResource) {
+        if (dataSource == DataSource.MEMORY_CACHE || !isFirstResource) {
+            return NoTransition.get();
+        }
+        if (animation == null) {
+            animation = new ViewPropertyTransition<>(animator);
+        }
+        return animation;
+    }
 }
