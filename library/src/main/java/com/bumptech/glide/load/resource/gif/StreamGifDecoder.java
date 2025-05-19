45efeafac089562b7f249899b1d0559488e52e94
Nima Karimipour <karimipour.nima@gmail.com>
2025-05-18 17:32:10 -0700

    fix: 68 - UNBOX_NULLABLE - return !options.get(GifOptions.DISABLE_ANIMATION) - unboxing of a @Nullable value

--- library/src/main/java/com/bumptech/glide/load/resource/gif/StreamGifDecoder.java
+++ library/src/main/java/com/bumptech/glide/load/resource/gif/StreamGifDecoder.java
@@ -38,10 +38,11 @@ public class StreamGifDecoder implements ResourceDecoder<InputStream, GifDrawabl
   }
 
   @Override
-  public boolean handles(@NonNull InputStream source, @NonNull Options options) throws IOException {
-    return !options.get(GifOptions.DISABLE_ANIMATION)
-        && ImageHeaderParserUtils.getType(parsers, source, byteArrayPool) == ImageType.GIF;
-  }
+    public boolean handles(@NonNull InputStream source, @NonNull Options options) throws IOException {
+        Boolean disableAnimation = options.get(GifOptions.DISABLE_ANIMATION);
+        return disableAnimation != null && !disableAnimation
+            && ImageHeaderParserUtils.getType(parsers, source, byteArrayPool) == ImageType.GIF;
+    }
 
   @Nullable
   @Override
