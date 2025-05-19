8d77d5b4c8d4746faf300b6f2cd5f3dacfb3a0ba
Nima Karimipour <karimipour.nima@gmail.com>
2025-05-18 17:26:10 -0700

    fix: 17 - UNBOX_NULLABLE - options.get(Downsampler.ALLOW_HARDWARE_CONFIG) != null - unboxing of a @Nullable value

--- library/src/main/java/com/bumptech/glide/load/resource/DefaultOnHeaderDecodedListener.java
+++ library/src/main/java/com/bumptech/glide/load/resource/DefaultOnHeaderDecodedListener.java
@@ -44,16 +44,15 @@ public final class DefaultOnHeaderDecodedListener implements OnHeaderDecodedList
   @Nullable private final PreferredColorSpace preferredColorSpace;
 
   public DefaultOnHeaderDecodedListener(
-      int requestedWidth, int requestedHeight, @NonNull Options options) {
-    this.requestedWidth = requestedWidth;
-    this.requestedHeight = requestedHeight;
-    decodeFormat = options.get(Downsampler.DECODE_FORMAT);
-    strategy = options.get(DownsampleStrategy.OPTION);
-    isHardwareConfigAllowed =
-        options.get(Downsampler.ALLOW_HARDWARE_CONFIG) != null
-            && options.get(Downsampler.ALLOW_HARDWARE_CONFIG);
-    preferredColorSpace = options.get(Downsampler.PREFERRED_COLOR_SPACE);
-  }
+        int requestedWidth, int requestedHeight, @NonNull Options options) {
+      this.requestedWidth = requestedWidth;
+      this.requestedHeight = requestedHeight;
+      decodeFormat = options.get(Downsampler.DECODE_FORMAT);
+      strategy = options.get(DownsampleStrategy.OPTION);
+      Boolean allowHardwareConfig = options.get(Downsampler.ALLOW_HARDWARE_CONFIG);
+      isHardwareConfigAllowed = allowHardwareConfig != null && allowHardwareConfig;
+      preferredColorSpace = options.get(Downsampler.PREFERRED_COLOR_SPACE);
+    }
 
   @Override
   public void onHeaderDecoded(
