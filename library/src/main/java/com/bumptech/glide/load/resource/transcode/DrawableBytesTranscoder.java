142a93109caf598b1def368c39e713254b10bc53
Nima Karimipour <karimipour.nima@gmail.com>
2025-05-18 17:34:22 -0700

    fix: 85 - PASS_NULLABLE - return bitmapBytesTranscoder.transcode( - passing @Nullable parameter 'BitmapResource.obtain(((BitmapDrawable) drawable).getBitmap(), bitmapPool)' where @NonNull is required

--- library/src/main/java/com/bumptech/glide/load/resource/transcode/DrawableBytesTranscoder.java
+++ library/src/main/java/com/bumptech/glide/load/resource/transcode/DrawableBytesTranscoder.java
@@ -29,19 +29,21 @@ public final class DrawableBytesTranscoder implements ResourceTranscoder<Drawabl
     this.gifDrawableBytesTranscoder = gifDrawableBytesTranscoder;
   }
 
-  @Nullable
   @Override
-  public Resource<byte[]> transcode(
-      @NonNull Resource<Drawable> toTranscode, @NonNull Options options) {
-    Drawable drawable = toTranscode.get();
-    if (drawable instanceof BitmapDrawable) {
-      return bitmapBytesTranscoder.transcode(
-          BitmapResource.obtain(((BitmapDrawable) drawable).getBitmap(), bitmapPool), options);
-    } else if (drawable instanceof GifDrawable) {
-      return gifDrawableBytesTranscoder.transcode(toGifDrawableResource(toTranscode), options);
+    public Resource<byte[]> transcode(
+        @NonNull Resource<Drawable> toTranscode, @NonNull Options options) {
+      Drawable drawable = toTranscode.get();
+      if (drawable instanceof BitmapDrawable) {
+        Bitmap bitmap = ((BitmapDrawable) drawable).getBitmap();
+        if (bitmap != null) {
+          return bitmapBytesTranscoder.transcode(
+              BitmapResource.obtain(bitmap, bitmapPool), options);
+        }
+      } else if (drawable instanceof GifDrawable) {
+        return gifDrawableBytesTranscoder.transcode(toGifDrawableResource(toTranscode), options);
+      }
+      return null;
     }
-    return null;
-  }
 
   @SuppressWarnings("unchecked")
   @NonNull
