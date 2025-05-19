3e727c1b42ceae3d3b7469594c4fab3787692393
Nima Karimipour <karimipour.nima@gmail.com>
2025-05-18 17:34:17 -0700

    Revert "fix: 84 - UNBOX_NULLABLE - currentSize += size * sortedSizes.get(type).get(size) * adapter.getElementSizeInBytes(); - unboxing of a @Nullable value"
    This reverts commit d0dd652eb9de7e9cee437044d8c36070dc8e091d.

--- library/src/main/java/com/bumptech/glide/load/engine/bitmap_recycle/LruArrayPool.java
+++ library/src/main/java/com/bumptech/glide/load/engine/bitmap_recycle/LruArrayPool.java
@@ -209,18 +209,15 @@ public final class LruArrayPool implements ArrayPool {
 
   // VisibleForTesting
   int getCurrentSize() {
-      int currentSize = 0;
-      for (Class<?> type : sortedSizes.keySet()) {
-        for (Integer size : sortedSizes.get(type).keySet()) {
-          ArrayAdapterInterface<?> adapter = getAdapterFromType(type);
-          Integer count = sortedSizes.get(type).get(size);
-          if (count != null) {
-            currentSize += size * count * adapter.getElementSizeInBytes();
-          }
-        }
+    int currentSize = 0;
+    for (Class<?> type : sortedSizes.keySet()) {
+      for (Integer size : sortedSizes.get(type).keySet()) {
+        ArrayAdapterInterface<?> adapter = getAdapterFromType(type);
+        currentSize += size * sortedSizes.get(type).get(size) * adapter.getElementSizeInBytes();
       }
-      return currentSize;
     }
+    return currentSize;
+  }
 
   private static final class KeyPool extends BaseKeyPool<Key> {
 
