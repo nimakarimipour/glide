95da464cc7fd4061f6431719b11f80a3313cd9d8
Nima Karimipour <karimipour.nima@gmail.com>
2025-06-01 11:27:32 -0700

    Revert "fix: 23 - METHOD_NO_INIT - private ModelKey() {} - initializer method does not guarantee @NonNull field model (line 79) is initialized along all control-flow paths (remember to check for exceptions or early returns)."
    This reverts commit b650360035f29e48984cfb05e1e700cc4987d492.

--- library/src/main/java/com/bumptech/glide/load/model/ModelCache.java
+++ library/src/main/java/com/bumptech/glide/load/model/ModelCache.java
@@ -6,7 +6,6 @@ import androidx.annotation.VisibleForTesting;
 import com.bumptech.glide.util.LruCache;
 import com.bumptech.glide.util.Util;
 import java.util.Queue;
-import com.uber.nullaway.annotations.Initializer;
 
 /**
  * A simple cache that can be used by {@link ModelLoader} and {@link ModelLoaderFactory} to cache
@@ -95,7 +94,7 @@ public class ModelCache<A, B> {
 
     private ModelKey() {}
 
-    @Initializer private void init(A model, int width, int height) {
+    private void init(A model, int width, int height) {
       this.model = model;
       this.width = width;
       this.height = height;
