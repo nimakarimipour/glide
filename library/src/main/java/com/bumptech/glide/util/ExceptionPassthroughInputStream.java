1ee0c971bf22030fb3fae2d073200b57fe9f3421
Nima Karimipour <karimipour.nima@gmail.com>
2025-06-01 11:26:08 -0700

    fix: 5 - METHOD_NO_INIT - ExceptionPassthroughInputStream() { - initializer method does not guarantee @NonNull field wrapped (line 29) is initialized along all control-flow paths (remember to check for exceptions or early returns).

--- library/src/main/java/com/bumptech/glide/util/ExceptionPassthroughInputStream.java
+++ library/src/main/java/com/bumptech/glide/util/ExceptionPassthroughInputStream.java
@@ -26,7 +26,7 @@ public final class ExceptionPassthroughInputStream extends InputStream {
   @GuardedBy("POOL")
   private static final Queue<ExceptionPassthroughInputStream> POOL = Util.createQueue(0);
 
-  private InputStream wrapped;
+  @Nullable private InputStream wrapped;
   @Nullable private IOException exception;
 
   @NonNull
@@ -60,69 +60,95 @@ public final class ExceptionPassthroughInputStream extends InputStream {
   }
 
   @Override
-  public int available() throws IOException {
-    return wrapped.available();
-  }
-
-  @Override
-  public void close() throws IOException {
-    wrapped.close();
-  }
-
-  @Override
-  public void mark(int readLimit) {
-    wrapped.mark(readLimit);
-  }
-
-  @Override
-  public boolean markSupported() {
-    return wrapped.markSupported();
-  }
-
-  @Override
-  public int read() throws IOException {
-    try {
-      return wrapped.read();
-    } catch (IOException e) {
-      exception = e;
-      throw e;
+    public int available() throws IOException {
+      if (wrapped == null) {
+        throw new IOException("InputStream is not set");
+      }
+      return wrapped.available();
     }
-  }
 
   @Override
-  public int read(byte[] buffer) throws IOException {
-    try {
-      return wrapped.read(buffer);
-    } catch (IOException e) {
-      exception = e;
-      throw e;
+    public void close() throws IOException {
+      if (wrapped != null) {
+        wrapped.close();
+      }
     }
-  }
 
   @Override
-  public int read(byte[] buffer, int byteOffset, int byteCount) throws IOException {
-    try {
-      return wrapped.read(buffer, byteOffset, byteCount);
-    } catch (IOException e) {
-      exception = e;
-      throw e;
+    public void mark(int readLimit) {
+      if (wrapped != null) {
+        wrapped.mark(readLimit);
+      }
     }
-  }
 
   @Override
-  public synchronized void reset() throws IOException {
-    wrapped.reset();
-  }
-
-  @Override
-  public long skip(long byteCount) throws IOException {
-    try {
-      return wrapped.skip(byteCount);
-    } catch (IOException e) {
-      exception = e;
-      throw e;
+    public boolean markSupported() {
+      if (wrapped == null) {
+        throw new IllegalStateException("InputStream is not set.");
+      }
+      return wrapped.markSupported();
     }
-  }
+
+  @Override
+    public int read() throws IOException {
+      if (wrapped == null) {
+        throw new NullPointerException("InputStream wrapped is null");
+      }
+      try {
+        return wrapped.read();
+      } catch (IOException e) {
+        exception = e;
+        throw e;
+      }
+    }
+
+  @Override
+    public int read(byte[] buffer) throws IOException {
+      if (wrapped == null) {
+        throw new IOException("InputStream is not set.");
+      }
+      try {
+        return wrapped.read(buffer);
+      } catch (IOException e) {
+        exception = e;
+        throw e;
+      }
+    }
+
+  @Override
+    public int read(byte[] buffer, int byteOffset, int byteCount) throws IOException {
+      if (wrapped == null) {
+        throw new IOException("InputStream is not set.");
+      }
+      try {
+        return wrapped.read(buffer, byteOffset, byteCount);
+      } catch (IOException e) {
+        exception = e;
+        throw e;
+      }
+    }
+
+  @Override
+    public synchronized void reset() throws IOException {
+      if (wrapped != null) {
+        wrapped.reset();
+      } else {
+        throw new IOException("InputStream is null");
+      }
+    }
+
+  @Override
+    public long skip(long byteCount) throws IOException {
+      if (wrapped == null) {
+        throw new IOException("InputStream is not set");
+      }
+      try {
+        return wrapped.skip(byteCount);
+      } catch (IOException e) {
+        exception = e;
+        throw e;
+      }
+    }
 
   @Nullable
   public IOException getException() {
