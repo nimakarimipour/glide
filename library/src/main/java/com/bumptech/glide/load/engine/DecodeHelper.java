25108501c65cdd245191e8703a95b24f57db2081
Nima Karimipour <karimipour.nima@gmail.com>
2025-06-01 11:29:58 -0700

    fix: 44 - ASSIGN_FIELD_NULLABLE - glideContext = null; - assigning @Nullable expression to @NonNull field

--- library/src/main/java/com/bumptech/glide/load/engine/DecodeHelper.java
+++ library/src/main/java/com/bumptech/glide/load/engine/DecodeHelper.java
@@ -149,10 +149,12 @@ final class DecodeHelper<Transcode> {
     return getLoadPath(dataClass) != null;
   }
 
-  @Nullable
-  <Data> LoadPath<Data, ?, Transcode> getLoadPath(Class<Data> dataClass) {
-    return glideContext.getRegistry().getLoadPath(dataClass, resourceClass, transcodeClass);
-  }
+  @Nullable <Data> LoadPath<Data, ?, Transcode> getLoadPath(Class<Data> dataClass) {
+      if (glideContext == null) {
+        return null;
+      }
+      return glideContext.getRegistry().getLoadPath(dataClass, resourceClass, transcodeClass);
+    }
 
   boolean isScaleOnlyOrNoTransform() {
     return isScaleOnlyOrNoTransform;
