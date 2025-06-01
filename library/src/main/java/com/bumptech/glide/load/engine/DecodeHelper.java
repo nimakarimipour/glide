86ca24c90d1898be617143afe64c0d2822e981fd
Nima Karimipour <karimipour.nima@gmail.com>
2025-06-01 11:30:10 -0700

    Revert "fix: 46 - ASSIGN_FIELD_NULLABLE - resourceClass = null; - assigning @Nullable expression to @NonNull field"
    This reverts commit c1ca3baaf89ccee1e010d5a2b1c8f6f3c8dd96dd.

--- library/src/main/java/com/bumptech/glide/load/engine/DecodeHelper.java
+++ library/src/main/java/com/bumptech/glide/load/engine/DecodeHelper.java
@@ -32,7 +32,7 @@ final class DecodeHelper<Transcode> {
   private Object model;
   private int width;
   private int height;
-  @Nullable private Class<?> resourceClass;
+  private Class<?> resourceClass;
   private DecodeJob.DiskCacheProvider diskCacheProvider;
   private Options options;
   @Nullable private Map<Class<?>, Transformation<?>> transformations;
