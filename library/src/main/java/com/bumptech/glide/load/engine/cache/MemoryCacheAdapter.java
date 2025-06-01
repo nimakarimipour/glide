eb89f05b92003e05567a18bf8afa165367e2bf42
Nima Karimipour <karimipour.nima@gmail.com>
2025-06-01 11:32:20 -0700

    Revert "fix: 61 - DEREFERENCE_NULLABLE - listener.onResourceRemoved(resource); - dereferenced expression listener is @Nullable"
    This reverts commit 7e0897197f2978ec8b5232ef9d52bdf5178e2b9d.

--- library/src/main/java/com/bumptech/glide/load/engine/cache/MemoryCacheAdapter.java
+++ library/src/main/java/com/bumptech/glide/load/engine/cache/MemoryCacheAdapter.java
@@ -31,13 +31,14 @@ public class MemoryCacheAdapter implements MemoryCache {
     return null;
   }
 
-  @Nullable @Override
-    public Resource<?> put(@NonNull Key key, Resource<?> resource) {
-      if (listener != null && resource != null) {
-        listener.onResourceRemoved(resource);
-      }
-      return null;
+  @Nullable
+  @Override
+  public Resource<?> put(@NonNull Key key, @Nullable Resource<?> resource) {
+    if (resource != null) {
+      listener.onResourceRemoved(resource);
     }
+    return null;
+  }
 
   @Override
   public void setResourceRemovedListener(@NonNull ResourceRemovedListener listener) {
