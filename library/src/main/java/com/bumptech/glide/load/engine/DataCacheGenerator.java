d2135a6a1588fe51343570f443fb9cb97ed63115
Nima Karimipour <karimipour.nima@gmail.com>
2025-06-01 11:29:05 -0700

    Revert "fix: 36 - DEREFERENCE_NULLABLE - cb.onDataFetcherFailed(sourceKey, e, loadData.fetcher, DataSource.DATA_DISK_CACHE); - dereferenced expression loadData is @Nullable"
    This reverts commit 5e995ebe649c426daf8764241d69803f573a4033.

--- library/src/main/java/com/bumptech/glide/load/engine/DataCacheGenerator.java
+++ library/src/main/java/com/bumptech/glide/load/engine/DataCacheGenerator.java
@@ -46,47 +46,45 @@ class DataCacheGenerator implements DataFetcherGenerator, DataFetcher.DataCallba
   }
 
   @Override
-    public boolean startNext() {
-      GlideTrace.beginSection("DataCacheGenerator.startNext");
-      try {
-        while (modelLoaders == null || !hasNextModelLoader()) {
-          sourceIdIndex++;
-          if (sourceIdIndex >= cacheKeys.size()) {
-            return false;
-          }
-  
-          Key sourceId = cacheKeys.get(sourceIdIndex);
-          // PMD.AvoidInstantiatingObjectsInLoops The loop iterates a limited number of times
-          // and the actions it performs are much more expensive than a single allocation.
-          @SuppressWarnings("PMD.AvoidInstantiatingObjectsInLoops")
-          Key originalKey = new DataCacheKey(sourceId, helper.getSignature());
-          cacheFile = helper.getDiskCache().get(originalKey);
-          if (cacheFile != null) {
-            this.sourceKey = sourceId;
-            modelLoaders = helper.getModelLoaders(cacheFile);
-            modelLoaderIndex = 0;
-          }
+  public boolean startNext() {
+    GlideTrace.beginSection("DataCacheGenerator.startNext");
+    try {
+      while (modelLoaders == null || !hasNextModelLoader()) {
+        sourceIdIndex++;
+        if (sourceIdIndex >= cacheKeys.size()) {
+          return false;
         }
-  
-        loadData = null;
-        boolean started = false;
-        while (!started && hasNextModelLoader()) {
-          ModelLoader<File, ?> modelLoader = modelLoaders.get(modelLoaderIndex++);
-          if (cacheFile != null) {
-            loadData =
-                modelLoader.buildLoadData(
-                    cacheFile, helper.getWidth(), helper.getHeight(), helper.getOptions());
-            if (loadData != null && helper.hasLoadPath(loadData.fetcher.getDataClass())) {
-              started = true;
-              loadData.fetcher.loadData(helper.getPriority(), this);
-            }
-          }
+
+        Key sourceId = cacheKeys.get(sourceIdIndex);
+        // PMD.AvoidInstantiatingObjectsInLoops The loop iterates a limited number of times
+        // and the actions it performs are much more expensive than a single allocation.
+        @SuppressWarnings("PMD.AvoidInstantiatingObjectsInLoops")
+        Key originalKey = new DataCacheKey(sourceId, helper.getSignature());
+        cacheFile = helper.getDiskCache().get(originalKey);
+        if (cacheFile != null) {
+          this.sourceKey = sourceId;
+          modelLoaders = helper.getModelLoaders(cacheFile);
+          modelLoaderIndex = 0;
         }
-        return started;
-      } finally {
-        GlideTrace.endSection();
       }
+
+      loadData = null;
+      boolean started = false;
+      while (!started && hasNextModelLoader()) {
+        ModelLoader<File, ?> modelLoader = modelLoaders.get(modelLoaderIndex++);
+        loadData =
+            modelLoader.buildLoadData(
+                cacheFile, helper.getWidth(), helper.getHeight(), helper.getOptions());
+        if (loadData != null && helper.hasLoadPath(loadData.fetcher.getDataClass())) {
+          started = true;
+          loadData.fetcher.loadData(helper.getPriority(), this);
+        }
+      }
+      return started;
+    } finally {
+      GlideTrace.endSection();
     }
+  }
 
   private boolean hasNextModelLoader() {
     return modelLoaderIndex < modelLoaders.size();
@@ -101,18 +99,12 @@ class DataCacheGenerator implements DataFetcherGenerator, DataFetcher.DataCallba
   }
 
   @Override
-    public void onDataReady(Object data) {
-      LoadData<?> local = loadData;
-      if (local != null) {
-        cb.onDataFetcherReady(sourceKey, data, local.fetcher, DataSource.DATA_DISK_CACHE, sourceKey);
-      }
-    }
+  public void onDataReady(@Nullable Object data) {
+    cb.onDataFetcherReady(sourceKey, data, loadData.fetcher, DataSource.DATA_DISK_CACHE, sourceKey);
+  }
 
   @Override
-    public void onLoadFailed(@NonNull Exception e) {
-      LoadData<?> local = loadData;
-      if (local != null) {
-        cb.onDataFetcherFailed(sourceKey, e, local.fetcher, DataSource.DATA_DISK_CACHE);
-      }
-    }
+  public void onLoadFailed(@NonNull Exception e) {
+    cb.onDataFetcherFailed(sourceKey, e, loadData.fetcher, DataSource.DATA_DISK_CACHE);
+  }
 }
