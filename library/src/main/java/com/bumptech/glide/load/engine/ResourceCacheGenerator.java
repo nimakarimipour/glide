7d97a3e7b18419a8691363656d9bb76a5989f947
Nima Karimipour <karimipour.nima@gmail.com>
2025-06-01 11:27:49 -0700

    Revert "fix: 24 - DEREFERENCE_NULLABLE - return modelLoaderIndex < modelLoaders.size(); - dereferenced expression modelLoaders is @Nullable"
    This reverts commit 96b1af82da0bc5cef976a1b7fa253fe50c276116.

--- library/src/main/java/com/bumptech/glide/load/engine/ResourceCacheGenerator.java
+++ library/src/main/java/com/bumptech/glide/load/engine/ResourceCacheGenerator.java
@@ -43,85 +43,81 @@ class ResourceCacheGenerator implements DataFetcherGenerator, DataFetcher.DataCa
 
   // See TODO below.
   @SuppressWarnings("PMD.CollapsibleIfStatements")
-    @Override
-    public boolean startNext() {
-      GlideTrace.beginSection("ResourceCacheGenerator.startNext");
-      try {
-        List<Key> sourceIds = helper.getCacheKeys();
-        if (sourceIds.isEmpty()) {
+  @Override
+  public boolean startNext() {
+    GlideTrace.beginSection("ResourceCacheGenerator.startNext");
+    try {
+      List<Key> sourceIds = helper.getCacheKeys();
+      if (sourceIds.isEmpty()) {
+        return false;
+      }
+      List<Class<?>> resourceClasses = helper.getRegisteredResourceClasses();
+      if (resourceClasses.isEmpty()) {
+        if (File.class.equals(helper.getTranscodeClass())) {
           return false;
         }
-        List<Class<?>> resourceClasses = helper.getRegisteredResourceClasses();
-        if (resourceClasses.isEmpty()) {
-          if (File.class.equals(helper.getTranscodeClass())) {
+        throw new IllegalStateException(
+            "Failed to find any load path from "
+                + helper.getModelClass()
+                + " to "
+                + helper.getTranscodeClass());
+      }
+      while (modelLoaders == null || !hasNextModelLoader()) {
+        resourceClassIndex++;
+        if (resourceClassIndex >= resourceClasses.size()) {
+          sourceIdIndex++;
+          if (sourceIdIndex >= sourceIds.size()) {
             return false;
           }
-          throw new IllegalStateException(
-              "Failed to find any load path from "
-                  + helper.getModelClass()
-                  + " to "
-                  + helper.getTranscodeClass());
+          resourceClassIndex = 0;
         }
-        while (modelLoaders == null || !hasNextModelLoader()) {
-          resourceClassIndex++;
-          if (resourceClassIndex >= resourceClasses.size()) {
-            sourceIdIndex++;
-            if (sourceIdIndex >= sourceIds.size()) {
-              return false;
-            }
-            resourceClassIndex = 0;
-          }
-  
-          Key sourceId = sourceIds.get(sourceIdIndex);
-          Class<?> resourceClass = resourceClasses.get(resourceClassIndex);
-          Transformation<?> transformation = helper.getTransformation(resourceClass);
-          // PMD.AvoidInstantiatingObjectsInLoops Each iteration is comparatively expensive anyway,
-          // we only run until the first one succeeds, the loop runs for only a limited
-          // number of iterations on the order of 10-20 in the worst case.
-          currentKey =
-              new ResourceCacheKey( // NOPMD AvoidInstantiatingObjectsInLoops
-                  helper.getArrayPool(),
-                  sourceId,
-                  helper.getSignature(),
-                  helper.getWidth(),
-                  helper.getHeight(),
-                  transformation,
-                  resourceClass,
-                  helper.getOptions());
-          cacheFile = helper.getDiskCache().get(currentKey);
-          if (cacheFile != null) {
-            sourceKey = sourceId;
-            modelLoaders = helper.getModelLoaders(cacheFile);
-            modelLoaderIndex = 0;
-          }
+
+        Key sourceId = sourceIds.get(sourceIdIndex);
+        Class<?> resourceClass = resourceClasses.get(resourceClassIndex);
+        Transformation<?> transformation = helper.getTransformation(resourceClass);
+        // PMD.AvoidInstantiatingObjectsInLoops Each iteration is comparatively expensive anyway,
+        // we only run until the first one succeeds, the loop runs for only a limited
+        // number of iterations on the order of 10-20 in the worst case.
+        currentKey =
+            new ResourceCacheKey( // NOPMD AvoidInstantiatingObjectsInLoops
+                helper.getArrayPool(),
+                sourceId,
+                helper.getSignature(),
+                helper.getWidth(),
+                helper.getHeight(),
+                transformation,
+                resourceClass,
+                helper.getOptions());
+        cacheFile = helper.getDiskCache().get(currentKey);
+        if (cacheFile != null) {
+          sourceKey = sourceId;
+          modelLoaders = helper.getModelLoaders(cacheFile);
+          modelLoaderIndex = 0;
         }
-  
-        if (cacheFile == null) {
-          return false;
-        }
-  
-        loadData = null;
-        boolean started = false;
-        while (!started && hasNextModelLoader()) {
-          ModelLoader<File, ?> modelLoader = modelLoaders.get(modelLoaderIndex++);
-          loadData =
-              modelLoader.buildLoadData(
-                  cacheFile, helper.getWidth(), helper.getHeight(), helper.getOptions());
-          if (loadData != null && helper.hasLoadPath(loadData.fetcher.getDataClass())) {
-            started = true;
-            loadData.fetcher.loadData(helper.getPriority(), this);
-          }
-        }
-  
-        return started;
-      } finally {
-        GlideTrace.endSection();
       }
+
+      loadData = null;
+      boolean started = false;
+      while (!started && hasNextModelLoader()) {
+        ModelLoader<File, ?> modelLoader = modelLoaders.get(modelLoaderIndex++);
+        loadData =
+            modelLoader.buildLoadData(
+                cacheFile, helper.getWidth(), helper.getHeight(), helper.getOptions());
+        if (loadData != null && helper.hasLoadPath(loadData.fetcher.getDataClass())) {
+          started = true;
+          loadData.fetcher.loadData(helper.getPriority(), this);
+        }
+      }
+
+      return started;
+    } finally {
+      GlideTrace.endSection();
     }
+  }
 
   private boolean hasNextModelLoader() {
-        return modelLoaders != null && modelLoaderIndex < modelLoaders.size();
-    }
+    return modelLoaderIndex < modelLoaders.size();
+  }
 
   @Override
   public void cancel() {
