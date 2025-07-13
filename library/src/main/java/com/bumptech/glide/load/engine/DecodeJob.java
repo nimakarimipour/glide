556718dced42383e7888fd5262f76644002dd3bc
Nima Karimipour <karimipour.nima@gmail.com>
2025-07-13 09:50:59 -0700

    before:55to53 - Type='DEREFERENCE_NULLABLE', message='dereferenced expression priority is @Nullable' /home/nima/Developer/nullness-benchmarks/glide/library/src/main/java/com/bumptech/glide/load/engine/DecodeJob.java:211     return priority.ordinal();

--- library/src/main/java/com/bumptech/glide/load/engine/DecodeJob.java
+++ library/src/main/java/com/bumptech/glide/load/engine/DecodeJob.java
@@ -87,54 +87,57 @@ class DecodeJob<R>
   }
 
   @Initializer
-  DecodeJob<R> init(
-      GlideContext glideContext,
-      @Nullable Object model,
-      EngineKey loadKey,
-      Key signature,
-      int width,
-      int height,
-      Class<?> resourceClass,
-      Class<R> transcodeClass,
-      Priority priority,
-      DiskCacheStrategy diskCacheStrategy,
-      Map<Class<?>, Transformation<?>> transformations,
-      boolean isTransformationRequired,
-      boolean isScaleOnlyOrNoTransform,
-      boolean onlyRetrieveFromCache,
-      Options options,
-      Callback<R> callback,
-      int order) {
-    decodeHelper.init(
-        glideContext,
-        model,
-        signature,
-        width,
-        height,
-        diskCacheStrategy,
-        resourceClass,
-        transcodeClass,
-        priority,
-        options,
-        transformations,
-        isTransformationRequired,
-        isScaleOnlyOrNoTransform,
-        diskCacheProvider);
-    this.glideContext = glideContext;
-    this.signature = signature;
-    this.priority = priority;
-    this.loadKey = loadKey;
-    this.width = width;
-    this.height = height;
-    this.diskCacheStrategy = diskCacheStrategy;
-    this.onlyRetrieveFromCache = onlyRetrieveFromCache;
-    this.options = options;
-    this.callback = callback;
-    this.order = order;
-    this.runReason = RunReason.INITIALIZE;
-    this.model = model;
-    return this;
-  }
+    DecodeJob<R> init(
+        GlideContext glideContext,
+         @Nullable Object model,
+        EngineKey loadKey,
+        Key signature,
+        int width,
+        int height,
+        Class<?> resourceClass,
+        Class<R> transcodeClass,
+        Priority priority,
+        DiskCacheStrategy diskCacheStrategy,
+        Map<Class<?>, Transformation<?>> transformations,
+        boolean isTransformationRequired,
+        boolean isScaleOnlyOrNoTransform,
+        boolean onlyRetrieveFromCache,
+        Options options,
+        Callback<R> callback,
+        int order) {
+      if (model == null) {
+        throw new IllegalArgumentException("Model cannot be null");
+      }
+      decodeHelper.init(
+          glideContext,
+          model,
+          signature,
+          width,
+          height,
+          diskCacheStrategy,
+          resourceClass,
+          transcodeClass,
+          priority,
+          options,
+          transformations,
+          isTransformationRequired,
+          isScaleOnlyOrNoTransform,
+          diskCacheProvider);
+      this.glideContext = glideContext;
+      this.signature = signature;
+      this.priority = priority;
+      this.loadKey = loadKey;
+      this.width = width;
+      this.height = height;
+      this.diskCacheStrategy = diskCacheStrategy;
+      this.onlyRetrieveFromCache = onlyRetrieveFromCache;
+      this.options = options;
+      this.callback = callback;
+      this.order = order;
+      this.runReason = RunReason.INITIALIZE;
+      this.model = model;
+      return this;
+    }
 
   /**
    * Returns true if this job will attempt to decode a resource from the disk cache, and false if it
@@ -209,8 +212,11 @@ class DecodeJob<R>
   }
 
   private int getPriority() {
-    return priority.ordinal();
-  }
+        if (priority == null) {
+            throw new IllegalStateException("Priority is not initialized");
+        }
+        return priority.ordinal();
+    }
 
   public void cancel() {
     isCancelled = true;
