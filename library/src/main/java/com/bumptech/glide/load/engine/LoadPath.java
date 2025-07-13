db479359a3532bb2f0f0610c12f3f5df95f107c5
Nima Karimipour <karimipour.nima@gmail.com>
2025-07-13 15:03:21 -0700

    Revert "fix: 33 - DEREFERENCE_NULLABLE - List<Throwable> throwables = Preconditions.checkNotNull(listPool.acquire()); - dereferenced expression listPool is @Nullable"
    This reverts commit 8aa62635f6738bf8220d05f4295c62190a5d6c26.

--- library/src/main/java/com/bumptech/glide/load/engine/LoadPath.java
+++ library/src/main/java/com/bumptech/glide/load/engine/LoadPath.java
@@ -47,20 +47,19 @@ public class LoadPath<Data, ResourceType, Transcode> {
   }
 
   public Resource<Transcode> load(
-        DataRewinder<Data> rewinder,
-        @NonNull Options options,
-        int width,
-        int height,
-        DecodePath.DecodeCallback<ResourceType> decodeCallback)
-        throws GlideException {
-      Preconditions.checkNotNull(listPool, "listPool must not be null");
-      List<Throwable> throwables = Preconditions.checkNotNull(listPool.acquire());
-      try {
-        return loadWithExceptionList(rewinder, options, width, height, decodeCallback, throwables);
-      } finally {
-        listPool.release(throwables);
-      }
+      DataRewinder<Data> rewinder,
+      @NonNull Options options,
+      int width,
+      int height,
+      DecodePath.DecodeCallback<ResourceType> decodeCallback)
+      throws GlideException {
+    List<Throwable> throwables = Preconditions.checkNotNull(listPool.acquire());
+    try {
+      return loadWithExceptionList(rewinder, options, width, height, decodeCallback, throwables);
+    } finally {
+      listPool.release(throwables);
     }
+  }
 
   private Resource<Transcode> loadWithExceptionList(
       DataRewinder<Data> rewinder,
