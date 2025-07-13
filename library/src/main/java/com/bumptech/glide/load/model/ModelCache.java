31ccc3e784901a0d74e8a79780870e5be4f401b7
Nima Karimipour <karimipour.nima@gmail.com>
2025-07-13 09:48:48 -0700

    before:69to68 - Type='METHOD_NO_INIT', message='initializer method does not guarantee @NonNull field model (line 79) is initialized along all control-flow paths (remember to check for exceptions or early returns).' /home/nima/Developer/nullness-benchmarks/glide/library/src/main/java/com/bumptech/glide/load/model/ModelCache.java:95     private ModelKey() {}

--- library/src/main/java/com/bumptech/glide/load/model/ModelCache.java
+++ library/src/main/java/com/bumptech/glide/load/model/ModelCache.java
@@ -6,6 +6,7 @@ import androidx.annotation.VisibleForTesting;
 import com.bumptech.glide.util.LruCache;
 import com.bumptech.glide.util.Util;
 import java.util.Queue;
+import com.uber.nullaway.annotations.Initializer;
 
 /**
  * A simple cache that can be used by {@link ModelLoader} and {@link ModelLoaderFactory} to cache
@@ -76,7 +77,7 @@ public class ModelCache<A, B> {
 
     private int height;
     private int width;
-    private A model;
+    @SuppressWarnings("NullAway.Init") private A model;
 
     @SuppressWarnings("unchecked")
     static <A> ModelKey<A> get(A model, int width, int height) {
@@ -94,7 +95,7 @@ public class ModelCache<A, B> {
 
     private ModelKey() {}
 
-    private void init(A model, int width, int height) {
+    @Initializer private void init(A model, int width, int height) {
       this.model = model;
       this.width = width;
       this.height = height;
