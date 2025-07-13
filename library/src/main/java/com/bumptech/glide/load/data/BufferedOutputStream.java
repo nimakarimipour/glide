4b6d85807f6aa1686d09a63c48b4b8609a9b4c66
Nima Karimipour <karimipour.nima@gmail.com>
2025-07-13 15:09:45 -0700

    fix: 68 - ASSIGN_FIELD_NULLABLE - buffer = null; - assigning @Nullable expression to @NonNull field

--- library/src/main/java/com/bumptech/glide/load/data/BufferedOutputStream.java
+++ library/src/main/java/com/bumptech/glide/load/data/BufferedOutputStream.java
@@ -5,6 +5,8 @@ import androidx.annotation.VisibleForTesting;
 import com.bumptech.glide.load.engine.bitmap_recycle.ArrayPool;
 import java.io.IOException;
 import java.io.OutputStream;
+import javax.annotation.Nullable;
+import edu.ucr.cs.riple.annotator.util.Nullability;
 
 /**
  * An {@link OutputStream} implementation that recycles and re-uses {@code byte[]}s using the
@@ -12,7 +14,7 @@ import java.io.OutputStream;
  */
 public final class BufferedOutputStream extends OutputStream {
   @NonNull private final OutputStream out;
-  private byte[] buffer;
+  @Nullable private byte[] buffer;
   private ArrayPool arrayPool;
   private int index;
 
@@ -28,10 +30,14 @@ public final class BufferedOutputStream extends OutputStream {
   }
 
   @Override
-  public void write(int b) throws IOException {
-    buffer[index++] = (byte) b;
-    maybeFlushBuffer();
-  }
+    public void write(int b) throws IOException {
+        if (buffer != null) {
+            Nullability.castToNonnull(buffer, "checked not null previously")[index++] = (byte) b;
+            maybeFlushBuffer();
+        } else {
+            throw new IOException("Buffer is not initialized.");
+        }
+    }
 
   @Override
   public void write(@NonNull byte[] b) throws IOException {
@@ -39,28 +45,31 @@ public final class BufferedOutputStream extends OutputStream {
   }
 
   @Override
-  public void write(@NonNull byte[] b, int initialOffset, int length) throws IOException {
-    int writtenSoFar = 0;
-    do {
-      int remainingToWrite = length - writtenSoFar;
-      int currentOffset = initialOffset + writtenSoFar;
-      // If we still need to write at least the buffer size worth of bytes, we might as well do so
-      // directly and avoid the overhead of copying to the buffer first.
-      if (index == 0 && remainingToWrite >= buffer.length) {
-        out.write(b, currentOffset, remainingToWrite);
-        return;
-      }
-
-      int remainingSpaceInBuffer = buffer.length - index;
-      int totalBytesToWriteToBuffer = Math.min(remainingToWrite, remainingSpaceInBuffer);
-
-      System.arraycopy(b, currentOffset, buffer, index, totalBytesToWriteToBuffer);
-
-      index += totalBytesToWriteToBuffer;
-      writtenSoFar += totalBytesToWriteToBuffer;
-
-      maybeFlushBuffer();
-    } while (writtenSoFar < length);
+        public void write(@NonNull byte[] b, int initialOffset, int length) throws IOException {
+            int writtenSoFar = 0;
+            do {
+              int remainingToWrite = length - writtenSoFar;
+              int currentOffset = initialOffset + writtenSoFar;
+        
+              if (buffer == null) {
+                throw new IOException("Buffer is not initialized.");
+              }
+        
+              if (index == 0 && remainingToWrite >= Nullability.castToNonnull(buffer, "buffer is checked")) {
+                out.write(b, currentOffset, remainingToWrite);
+                return;
+              }
+        
+              int remainingSpaceInBuffer = Nullability.castToNonnull(buffer, "buffer is checked").length - index;
+              int totalBytesToWriteToBuffer = Math.min(remainingToWrite, remainingSpaceInBuffer);
+        
+              System.arraycopy(b, currentOffset, buffer, index, totalBytesToWriteToBuffer);
+        
+              index += totalBytesToWriteToBuffer;
+              writtenSoFar += totalBytesToWriteToBuffer;
+        
+              maybeFlushBuffer();
+            } while (writtenSoFar < length);
   }
 
   @Override
@@ -77,10 +86,10 @@ public final class BufferedOutputStream extends OutputStream {
   }
 
   private void maybeFlushBuffer() throws IOException {
-    if (index == buffer.length) {
-      flushBuffer();
+        if (buffer != null && index == buffer.length) {
+            flushBuffer();
+        }
     }
-  }
 
   @Override
   public void close() throws IOException {
