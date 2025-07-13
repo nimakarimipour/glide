6d56b97c74ed04cb445f87d48ad1dfc73b2e90c3
Nima Karimipour <karimipour.nima@gmail.com>
2025-07-13 09:40:01 -0700

    before:51to50 - Type='UNBOX_NULLABLE', message='unboxing of a @Nullable value - countForResult' /home/nima/Developer/nullness-benchmarks/glide/library/src/main/java/com/bumptech/glide/load/engine/prefill/PreFillQueue.java:28     if (countForResult == 1) {

--- library/src/main/java/com/bumptech/glide/load/engine/prefill/PreFillQueue.java
+++ library/src/main/java/com/bumptech/glide/load/engine/prefill/PreFillQueue.java
@@ -22,22 +22,21 @@ final class PreFillQueue {
   }
 
   public PreFillType remove() {
-    PreFillType result = keyList.get(keyIndex);
-
-    Integer countForResult = bitmapsPerType.get(result);
-    if (countForResult == 1) {
-      bitmapsPerType.remove(result);
-      keyList.remove(keyIndex);
-    } else {
-      bitmapsPerType.put(result, countForResult - 1);
+      PreFillType result = keyList.get(keyIndex);
+  
+      Integer countForResult = bitmapsPerType.get(result);
+      if (countForResult != null && countForResult == 1) {
+        bitmapsPerType.remove(result);
+        keyList.remove(keyIndex);
+      } else if (countForResult != null) {
+        bitmapsPerType.put(result, countForResult - 1);
+      }
+      bitmapsRemaining--;
+  
+      keyIndex = keyList.isEmpty() ? 0 : (keyIndex + 1) % keyList.size();
+  
+      return result;
     }
-    bitmapsRemaining--;
-
-    // Avoid divide by 0.
-    keyIndex = keyList.isEmpty() ? 0 : (keyIndex + 1) % keyList.size();
-
-    return result;
-  }
 
   public int getSize() {
     return bitmapsRemaining;
