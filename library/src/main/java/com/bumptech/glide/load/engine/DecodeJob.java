bfe1a6816372f5103f0b588da753da2f8e0cc24a
Nima Karimipour <karimipour.nima@gmail.com>
2025-07-13 09:38:17 -0700

    before:64to63 - Type='DEREFERENCE_NULLABLE', message='dereferenced expression priority is @Nullable' /home/nima/Developer/nullness-benchmarks/glide/library/src/main/java/com/bumptech/glide/load/engine/DecodeJob.java:211     return priority.ordinal();

--- library/src/main/java/com/bumptech/glide/load/engine/DecodeJob.java
+++ library/src/main/java/com/bumptech/glide/load/engine/DecodeJob.java
@@ -208,8 +208,12 @@ class DecodeJob<R>
   }
 
   private int getPriority() {
-    return priority.ordinal();
-  }
+      if (priority == null) {
+        // Return a default value or handle the null case as needed
+        return 0; // Example default value
+      }
+      return priority.ordinal();
+    }
 
   public void cancel() {
     isCancelled = true;
