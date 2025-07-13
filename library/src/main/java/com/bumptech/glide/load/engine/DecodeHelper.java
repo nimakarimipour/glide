976f5aabb34c0d05cf0aaf5a411634f5bf813eb9
Nima Karimipour <karimipour.nima@gmail.com>
2025-07-13 14:52:56 -0700

    fix: 43 - ASSIGN_FIELD_NULLABLE - model = null; - assigning @Nullable expression to @NonNull field

--- library/src/main/java/com/bumptech/glide/load/engine/DecodeHelper.java
+++ library/src/main/java/com/bumptech/glide/load/engine/DecodeHelper.java
@@ -79,21 +79,23 @@ final class DecodeHelper<Transcode> {
   }
 
   void clear() {
-    glideContext = null;
-    model = null;
-    signature = null;
-    resourceClass = null;
-    transcodeClass = null;
-    options = null;
-    priority = null;
-    transformations = null;
-    diskCacheStrategy = null;
-
-    loadData.clear();
-    isLoadDataSet = false;
-    cacheKeys.clear();
-    isCacheKeysSet = false;
-  }
+      glideContext = null;
+      // Ensure model is Nullable before setting it to null
+      @Nullable Object modelNullable = model;
+      model = null;
+      signature = null;
+      resourceClass = null;
+      transcodeClass = null;
+      options = null;
+      priority = null;
+      transformations = null;
+      diskCacheStrategy = null;
+  
+      loadData.clear();
+      isLoadDataSet = false;
+      cacheKeys.clear();
+      isCacheKeysSet = false;
+    }
 
   DiskCache getDiskCache() {
     return diskCacheProvider.getDiskCache();
