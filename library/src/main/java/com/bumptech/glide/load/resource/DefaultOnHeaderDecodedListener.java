2c5f0f475d62dc8716741062b9dd9235d5060ba3
Nima Karimipour <karimipour.nima@gmail.com>
2025-07-13 14:49:45 -0700

    Revert "fix: 17 - UNBOX_NULLABLE - options.get(Downsampler.ALLOW_HARDWARE_CONFIG) != null - unboxing of a @Nullable value - options.get(Downsampler.ALLOW_HARDWARE_CONFIG)"
    This reverts commit db301d695a0dd72aeb4d968644950ec2f14862d0.

--- library/src/main/java/com/bumptech/glide/load/resource/DefaultOnHeaderDecodedListener.java
+++ library/src/main/java/com/bumptech/glide/load/resource/DefaultOnHeaderDecodedListener.java
@@ -44,16 +44,16 @@ public final class DefaultOnHeaderDecodedListener implements OnHeaderDecodedList
   @Nullable private final PreferredColorSpace preferredColorSpace;
 
   public DefaultOnHeaderDecodedListener(
-        int requestedWidth, int requestedHeight, @NonNull Options options) {
-      this.requestedWidth = requestedWidth;
-      this.requestedHeight = requestedHeight;
-      decodeFormat = options.get(Downsampler.DECODE_FORMAT);
-      strategy = options.get(DownsampleStrategy.OPTION);
-      Boolean hardwareConfigOption = options.get(Downsampler.ALLOW_HARDWARE_CONFIG);
-      isHardwareConfigAllowed =
-          hardwareConfigOption != null && hardwareConfigOption;
-      preferredColorSpace = options.get(Downsampler.PREFERRED_COLOR_SPACE);
-    }
+      int requestedWidth, int requestedHeight, @NonNull Options options) {
+    this.requestedWidth = requestedWidth;
+    this.requestedHeight = requestedHeight;
+    decodeFormat = options.get(Downsampler.DECODE_FORMAT);
+    strategy = options.get(DownsampleStrategy.OPTION);
+    isHardwareConfigAllowed =
+        options.get(Downsampler.ALLOW_HARDWARE_CONFIG) != null
+            && options.get(Downsampler.ALLOW_HARDWARE_CONFIG);
+    preferredColorSpace = options.get(Downsampler.PREFERRED_COLOR_SPACE);
+  }
 
   @Override
   public void onHeaderDecoded(
