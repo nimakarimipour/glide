f39690ac1db4dbfff99ea7701ab94fa39703da92
Nima Karimipour <karimipour.nima@gmail.com>
2025-07-13 14:52:21 -0700

    Revert "fix: 38 - DEREFERENCE_NULLABLE - int errorOverrideWidth = errorBuilder.getOverrideWidth(); - dereferenced expression errorBuilder is @Nullable"
    This reverts commit cf6c699dd2d4d58b02bf81af0857bf41b177bdd9.

--- library/src/main/java/com/bumptech/glide/RequestBuilder.java
+++ library/src/main/java/com/bumptech/glide/RequestBuilder.java
@@ -1086,63 +1086,63 @@ public class RequestBuilder<TranscodeType> extends BaseRequestOptions<RequestBui
   }
 
   private Request buildRequestRecursive(
-        Object requestLock,
-        Target<TranscodeType> target,
-        @Nullable RequestListener<TranscodeType> targetListener,
-        @Nullable RequestCoordinator parentCoordinator,
-        TransitionOptions<?, ? super TranscodeType> transitionOptions,
-        Priority priority,
-        int overrideWidth,
-        int overrideHeight,
-        BaseRequestOptions<?> requestOptions,
-        Executor callbackExecutor) {
-  
-      // Build the ErrorRequestCoordinator first if necessary so we can update parentCoordinator.
-      ErrorRequestCoordinator errorRequestCoordinator = null;
-      if (errorBuilder != null) {
-        errorRequestCoordinator = new ErrorRequestCoordinator(requestLock, parentCoordinator);
-        parentCoordinator = errorRequestCoordinator;
-      }
-  
-      Request mainRequest =
-          buildThumbnailRequestRecursive(
-              requestLock,
-              target,
-              targetListener,
-              parentCoordinator,
-              transitionOptions,
-              priority,
-              overrideWidth,
-              overrideHeight,
-              requestOptions,
-              callbackExecutor);
-  
-      if (errorRequestCoordinator == null || errorBuilder == null) {
-        return mainRequest;
-      }
-  
-      int errorOverrideWidth = errorBuilder.getOverrideWidth();
-      int errorOverrideHeight = errorBuilder.getOverrideHeight();
-      if (Util.isValidDimensions(overrideWidth, overrideHeight) && !errorBuilder.isValidOverride()) {
-        errorOverrideWidth = requestOptions.getOverrideWidth();
-        errorOverrideHeight = requestOptions.getOverrideHeight();
-      }
-  
-      Request errorRequest =
-          errorBuilder.buildRequestRecursive(
-              requestLock,
-              target,
-              targetListener,
-              errorRequestCoordinator,
-              errorBuilder.transitionOptions,
-              errorBuilder.getPriority(),
-              errorOverrideWidth,
-              errorOverrideHeight,
-              errorBuilder,
-              callbackExecutor);
-      errorRequestCoordinator.setRequests(mainRequest, errorRequest);
-      return errorRequestCoordinator;
+      Object requestLock,
+      Target<TranscodeType> target,
+      @Nullable RequestListener<TranscodeType> targetListener,
+      @Nullable RequestCoordinator parentCoordinator,
+      TransitionOptions<?, ? super TranscodeType> transitionOptions,
+      Priority priority,
+      int overrideWidth,
+      int overrideHeight,
+      BaseRequestOptions<?> requestOptions,
+      Executor callbackExecutor) {
+
+    // Build the ErrorRequestCoordinator first if necessary so we can update parentCoordinator.
+    ErrorRequestCoordinator errorRequestCoordinator = null;
+    if (errorBuilder != null) {
+      errorRequestCoordinator = new ErrorRequestCoordinator(requestLock, parentCoordinator);
+      parentCoordinator = errorRequestCoordinator;
     }
+
+    Request mainRequest =
+        buildThumbnailRequestRecursive(
+            requestLock,
+            target,
+            targetListener,
+            parentCoordinator,
+            transitionOptions,
+            priority,
+            overrideWidth,
+            overrideHeight,
+            requestOptions,
+            callbackExecutor);
+
+    if (errorRequestCoordinator == null) {
+      return mainRequest;
+    }
+
+    int errorOverrideWidth = errorBuilder.getOverrideWidth();
+    int errorOverrideHeight = errorBuilder.getOverrideHeight();
+    if (Util.isValidDimensions(overrideWidth, overrideHeight) && !errorBuilder.isValidOverride()) {
+      errorOverrideWidth = requestOptions.getOverrideWidth();
+      errorOverrideHeight = requestOptions.getOverrideHeight();
+    }
+
+    Request errorRequest =
+        errorBuilder.buildRequestRecursive(
+            requestLock,
+            target,
+            targetListener,
+            errorRequestCoordinator,
+            errorBuilder.transitionOptions,
+            errorBuilder.getPriority(),
+            errorOverrideWidth,
+            errorOverrideHeight,
+            errorBuilder,
+            callbackExecutor);
+    errorRequestCoordinator.setRequests(mainRequest, errorRequest);
+    return errorRequestCoordinator;
+  }
 
   private Request buildThumbnailRequestRecursive(
       Object requestLock,
