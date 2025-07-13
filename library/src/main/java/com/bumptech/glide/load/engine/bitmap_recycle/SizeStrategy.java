ed11285185fe8c5110b513886b5b605fd2862df0
Nima Karimipour <karimipour.nima@gmail.com>
2025-07-13 15:00:36 -0700

    Revert "fix: 14 - UNBOX_NULLABLE - if (current == 1) { - unboxing of a @Nullable value - current"
    This reverts commit d7fcbf5b7046e9bd18b74b5ad9a906ef63c365c5.

--- library/src/main/java/com/bumptech/glide/load/engine/bitmap_recycle/SizeStrategy.java
+++ library/src/main/java/com/bumptech/glide/load/engine/bitmap_recycle/SizeStrategy.java
@@ -8,7 +8,6 @@ import androidx.annotation.VisibleForTesting;
 import com.bumptech.glide.util.Synthetic;
 import com.bumptech.glide.util.Util;
 import java.util.NavigableMap;
-import edu.ucr.cs.riple.annotator.util.Nullability;
 
 /**
  * A strategy for reusing bitmaps that relies on {@link Bitmap#reconfigure(int, int,
@@ -69,12 +68,12 @@ final class SizeStrategy implements LruPoolStrategy {
   }
 
   private void decrementBitmapOfSize(Integer size) {
-      Integer current = Nullability.castToNonnull(sortedSizes.get(size));
-      if (current == 1) {
-        sortedSizes.remove(size);
-      } else {
-        sortedSizes.put(size, current - 1);
-      }
+    Integer current = sortedSizes.get(size);
+    if (current == 1) {
+      sortedSizes.remove(size);
+    } else {
+      sortedSizes.put(size, current - 1);
+    }
   }
 
   @Override
