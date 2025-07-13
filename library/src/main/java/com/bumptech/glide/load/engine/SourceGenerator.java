23195fee7688e497a4b5e661de1c46966d7e7a1b
Nima Karimipour <karimipour.nima@gmail.com>
2025-07-13 15:00:22 -0700

    Revert "fix: 12 - RETURN_NULLABLE - return priority; - returning @Nullable expression from method with @NonNull return type"
    This reverts commit d610b1e4dcfcba57596561481e654f7771705a7d.

--- library/src/main/java/com/bumptech/glide/load/engine/SourceGenerator.java
+++ library/src/main/java/com/bumptech/glide/load/engine/SourceGenerator.java
@@ -16,7 +16,6 @@ import com.bumptech.glide.util.LogTime;
 import com.bumptech.glide.util.Synthetic;
 import java.io.IOException;
 import java.util.Collections;
-import edu.ucr.cs.riple.annotator.util.Nullability;
 
 /**
  * Generates {@link com.bumptech.glide.load.data.DataFetcher DataFetchers} from original source data
@@ -93,24 +92,24 @@ class SourceGenerator implements DataFetcherGenerator, DataFetcherGenerator.Fetc
   }
 
   private void startNextLoad(final LoadData<?> toStart) {
-      loadData.fetcher.loadData(
-          Nullability.castToNonnull(helper.getPriority()),
-          new DataCallback<Object>() {
-            @Override
-            public void onDataReady( Object data) {
-              if (isCurrentRequest(toStart)) {
-                onDataReadyInternal(toStart, data);
-              }
+    loadData.fetcher.loadData(
+        helper.getPriority(),
+        new DataCallback<Object>() {
+          @Override
+          public void onDataReady(@Nullable Object data) {
+            if (isCurrentRequest(toStart)) {
+              onDataReadyInternal(toStart, data);
             }
-  
-            @Override
-            public void onLoadFailed(@NonNull Exception e) {
-              if (isCurrentRequest(toStart)) {
-                onLoadFailedInternal(toStart, e);
-              }
+          }
+
+          @Override
+          public void onLoadFailed(@NonNull Exception e) {
+            if (isCurrentRequest(toStart)) {
+              onLoadFailedInternal(toStart, e);
             }
-          });
-    }
+          }
+        });
+  }
 
   // We want reference equality explicitly to make sure we ignore results from old requests.
   @SuppressWarnings({"PMD.CompareObjectsWithEquals", "WeakerAccess"})
