7f01364a3f4cdb6c6b6b1b9fcb3f697e61ed5729
Nima Karimipour <karimipour.nima@gmail.com>
2025-07-13 15:00:44 -0700

    Revert "fix: 15 - UNBOX_NULLABLE - int timeout = options.get(TIMEOUT); - unboxing of a @Nullable value - options.get(TIMEOUT)"
    This reverts commit 2a780a8beaa4c86312f9a8fbfee0498df09f339a.

--- library/src/main/java/com/bumptech/glide/load/model/stream/HttpGlideUrlLoader.java
+++ library/src/main/java/com/bumptech/glide/load/model/stream/HttpGlideUrlLoader.java
@@ -11,7 +11,6 @@ import com.bumptech.glide.load.model.ModelLoader;
 import com.bumptech.glide.load.model.ModelLoaderFactory;
 import com.bumptech.glide.load.model.MultiModelLoaderFactory;
 import java.io.InputStream;
-import edu.ucr.cs.riple.annotator.util.Nullability;
 
 /**
  * An {@link com.bumptech.glide.load.model.ModelLoader} for translating {@link
@@ -40,18 +39,20 @@ public class HttpGlideUrlLoader implements ModelLoader<GlideUrl, InputStream> {
   }
 
   @Override
-    public LoadData<InputStream> buildLoadData(
-        @NonNull GlideUrl model, int width, int height, @NonNull Options options) {
-      GlideUrl url = model;
-      if (modelCache != null) {
-        url = modelCache.get(model, 0, 0);
-        if (url == null) {
-          modelCache.put(model, 0, 0, model);
-          url = model;
-        }
+  public LoadData<InputStream> buildLoadData(
+      @NonNull GlideUrl model, int width, int height, @NonNull Options options) {
+    // GlideUrls memoize parsed URLs so caching them saves a few object instantiations and time
+    // spent parsing urls.
+    GlideUrl url = model;
+    if (modelCache != null) {
+      url = modelCache.get(model, 0, 0);
+      if (url == null) {
+        modelCache.put(model, 0, 0, model);
+        url = model;
       }
-      int timeout = Nullability.castToNonnull(options.get(TIMEOUT));
-      return new LoadData<>(url, new HttpUrlFetcher(url, timeout));
+    }
+    int timeout = options.get(TIMEOUT);
+    return new LoadData<>(url, new HttpUrlFetcher(url, timeout));
   }
 
   @Override
