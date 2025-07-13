109be8a2dc7242914af1a2a06aaa66a714da57a0
Nima Karimipour <karimipour.nima@gmail.com>
2025-07-13 15:06:13 -0700

    fix: 48 - PASS_NULLABLE - modelLoader.buildLoadData( - passing @Nullable parameter 'cacheFile' where @NonNull is required

--- library/src/main/java/com/bumptech/glide/load/engine/ResourceCacheGenerator.java
+++ library/src/main/java/com/bumptech/glide/load/engine/ResourceCacheGenerator.java
@@ -11,6 +11,7 @@ import com.bumptech.glide.util.pool.GlideTrace;
 import java.io.File;
 import java.util.List;
 import javax.annotation.Nullable;
+import edu.ucr.cs.riple.annotator.util.Nullability;
 
 /**
  * Generates {@link com.bumptech.glide.load.data.DataFetcher DataFetchers} from cache files
@@ -43,76 +44,73 @@ class ResourceCacheGenerator implements DataFetcherGenerator, DataFetcher.DataCa
 
   // See TODO below.
   @SuppressWarnings("PMD.CollapsibleIfStatements")
-  @Override
-  public boolean startNext() {
-    GlideTrace.beginSection("ResourceCacheGenerator.startNext");
-    try {
-      List<Key> sourceIds = helper.getCacheKeys();
-      if (sourceIds.isEmpty()) {
-        return false;
-      }
-      List<Class<?>> resourceClasses = helper.getRegisteredResourceClasses();
-      if (resourceClasses.isEmpty()) {
-        if (File.class.equals(helper.getTranscodeClass())) {
+    @Override
+    public boolean startNext() {
+      GlideTrace.beginSection("ResourceCacheGenerator.startNext");
+      try {
+        List<Key> sourceIds = helper.getCacheKeys();
+        if (sourceIds.isEmpty()) {
           return false;
         }
-        throw new IllegalStateException(
-            "Failed to find any load path from "
-                + helper.getModelClass()
-                + " to "
-                + helper.getTranscodeClass());
-      }
-      while (modelLoaders == null || !hasNextModelLoader()) {
-        resourceClassIndex++;
-        if (resourceClassIndex >= resourceClasses.size()) {
-          sourceIdIndex++;
-          if (sourceIdIndex >= sourceIds.size()) {
+        List<Class<?>> resourceClasses = helper.getRegisteredResourceClasses();
+        if (resourceClasses.isEmpty()) {
+          if (File.class.equals(helper.getTranscodeClass())) {
             return false;
           }
-          resourceClassIndex = 0;
+          throw new IllegalStateException(
+              "Failed to find any load path from "
+                  + helper.getModelClass()
+                  + " to "
+                  + helper.getTranscodeClass());
         }
-
-        Key sourceId = sourceIds.get(sourceIdIndex);
-        Class<?> resourceClass = resourceClasses.get(resourceClassIndex);
-        Transformation<?> transformation = helper.getTransformation(resourceClass);
-        // PMD.AvoidInstantiatingObjectsInLoops Each iteration is comparatively expensive anyway,
-        // we only run until the first one succeeds, the loop runs for only a limited
-        // number of iterations on the order of 10-20 in the worst case.
-        currentKey =
-            new ResourceCacheKey( // NOPMD AvoidInstantiatingObjectsInLoops
-                helper.getArrayPool(),
-                sourceId,
-                helper.getSignature(),
-                helper.getWidth(),
-                helper.getHeight(),
-                transformation,
-                resourceClass,
-                helper.getOptions());
-        cacheFile = helper.getDiskCache().get(currentKey);
-        if (cacheFile != null) {
-          sourceKey = sourceId;
-          modelLoaders = helper.getModelLoaders(cacheFile);
-          modelLoaderIndex = 0;
+        while (modelLoaders == null || !hasNextModelLoader()) {
+          resourceClassIndex++;
+          if (resourceClassIndex >= resourceClasses.size()) {
+            sourceIdIndex++;
+            if (sourceIdIndex >= sourceIds.size()) {
+              return false;
+            }
+            resourceClassIndex = 0;
+          }
+  
+          Key sourceId = sourceIds.get(sourceIdIndex);
+          Class<?> resourceClass = resourceClasses.get(resourceClassIndex);
+          Transformation<?> transformation = helper.getTransformation(resourceClass);
+          currentKey =
+              new ResourceCacheKey( // NOPMD AvoidInstantiatingObjectsInLoops
+                  helper.getArrayPool(),
+                  sourceId,
+                  helper.getSignature(),
+                  helper.getWidth(),
+                  helper.getHeight(),
+                  transformation,
+                  resourceClass,
+                  helper.getOptions());
+          cacheFile = helper.getDiskCache().get(currentKey);
+          if (cacheFile != null) {
+            sourceKey = sourceId;
+            modelLoaders = helper.getModelLoaders(cacheFile);
+            modelLoaderIndex = 0;
+          }
         }
+  
+        loadData = null;
+        boolean started = false;
+        while (!started && hasNextModelLoader()) {
+          ModelLoader<File, ?> modelLoader = modelLoaders.get(modelLoaderIndex++);
+          loadData =
+              modelLoader.buildLoadData(
+                  Nullability.castToNonnull(cacheFile), helper.getWidth(), helper.getHeight(), helper.getOptions());
+          if (loadData != null && helper.hasLoadPath(loadData.fetcher.getDataClass())) {
+            started = true;
+            loadData.fetcher.loadData(helper.getPriority(), this);
+          }
+        }
+  
+        return started;
+      } finally {
+        GlideTrace.endSection();
       }
-
-      loadData = null;
-      boolean started = false;
-      while (!started && hasNextModelLoader()) {
-        ModelLoader<File, ?> modelLoader = modelLoaders.get(modelLoaderIndex++);
-        loadData =
-            modelLoader.buildLoadData(
-                cacheFile, helper.getWidth(), helper.getHeight(), helper.getOptions());
-        if (loadData != null && helper.hasLoadPath(loadData.fetcher.getDataClass())) {
-          started = true;
-          loadData.fetcher.loadData(helper.getPriority(), this);
-        }
-      }
-
-      return started;
-    } finally {
-      GlideTrace.endSection();
-    }
   }
 
   private boolean hasNextModelLoader() {
