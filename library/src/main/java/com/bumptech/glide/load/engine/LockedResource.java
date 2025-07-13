49c24bacbc72dbf42bf0a7d1936230764ac49413
Nima Karimipour <karimipour.nima@gmail.com>
2025-07-13 15:01:14 -0700

    Revert "fix: 19 - ASSIGN_FIELD_NULLABLE - toWrap = null; - assigning @Nullable expression to @NonNull field"
    This reverts commit d21f6686cde22138e814c7e6a385c4b43a161f9b.

--- library/src/main/java/com/bumptech/glide/load/engine/LockedResource.java
+++ library/src/main/java/com/bumptech/glide/load/engine/LockedResource.java
@@ -6,8 +6,6 @@ import com.bumptech.glide.util.Preconditions;
 import com.bumptech.glide.util.Synthetic;
 import com.bumptech.glide.util.pool.FactoryPools;
 import com.bumptech.glide.util.pool.StateVerifier;
-import javax.annotation.Nullable;
-import com.uber.nullaway.annotations.Initializer;
 
 /**
  * A resource that defers any calls to {@link Resource#recycle()} until after {@link #unlock()} is
@@ -43,7 +41,7 @@ final class LockedResource<Z> implements Resource<Z>, FactoryPools.Poolable {
   @Synthetic
   LockedResource() {}
 
-  @Initializer private void init(Resource<Z> toWrap) {
+  private void init(Resource<Z> toWrap) {
     isRecycled = false;
     isLocked = true;
     this.toWrap = toWrap;
