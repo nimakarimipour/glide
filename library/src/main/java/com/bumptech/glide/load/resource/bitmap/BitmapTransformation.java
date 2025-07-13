c00b71c835d79740ef14ab478c73627a68ddc016
Nima Karimipour <karimipour.nima@gmail.com>
2025-07-13 15:11:58 -0700

    fix: 82 - RETURN_NULLABLE - return result; - returning @Nullable expression from method with @NonNull return type

--- library/src/main/java/com/bumptech/glide/load/resource/bitmap/BitmapTransformation.java
+++ library/src/main/java/com/bumptech/glide/load/resource/bitmap/BitmapTransformation.java
@@ -11,6 +11,7 @@ import com.bumptech.glide.request.target.Target;
 import com.bumptech.glide.util.Util;
 import java.nio.charset.Charset;
 import java.security.MessageDigest;
+import javax.annotation.Nullable;
 
 /**
  * A simple {@link com.bumptech.glide.load.Transformation} for transforming {@link
@@ -64,7 +65,7 @@ import java.security.MessageDigest;
  */
 public abstract class BitmapTransformation implements Transformation<Bitmap> {
 
-  @NonNull
+  @Nullable @NonNull
   @Override
   public final Resource<Bitmap> transform(
       @NonNull Context context, @NonNull Resource<Bitmap> resource, int outWidth, int outHeight) {
