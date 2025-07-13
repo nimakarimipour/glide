bb1b083b7a051a7bb01f932940ca9aa954972dd7
Nima Karimipour <karimipour.nima@gmail.com>
2025-07-13 14:57:21 -0700

    fix: 79 - DEREFERENCE_NULLABLE - cb.onDataFetcherFailed(currentKey, e, loadData.fetcher, DataSource.RESOURCE_DISK_CACHE); - dereferenced expression loadData is @Nullable

--- library/src/main/java/com/bumptech/glide/load/engine/ResourceCacheGenerator.java
+++ library/src/main/java/com/bumptech/glide/load/engine/ResourceCacheGenerator.java
@@ -134,7 +134,11 @@ class ResourceCacheGenerator implements DataFetcherGenerator, DataFetcher.DataCa
   }
 
   @Override
-  public void onLoadFailed(@NonNull Exception e) {
-    cb.onDataFetcherFailed(currentKey, e, loadData.fetcher, DataSource.RESOURCE_DISK_CACHE);
-  }
+    public void onLoadFailed(@NonNull Exception e) {
+      if (loadData != null) {
+        cb.onDataFetcherFailed(currentKey, e, loadData.fetcher, DataSource.RESOURCE_DISK_CACHE);
+      } else {
+        cb.onDataFetcherFailed(currentKey, e, null, DataSource.RESOURCE_DISK_CACHE);
+      }
+    }
 }
