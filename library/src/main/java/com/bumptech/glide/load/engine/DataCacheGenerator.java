360b91bade637ada038fc9b1c150c382e6aa1597
Nima Karimipour <karimipour.nima@gmail.com>
2025-07-13 14:53:42 -0700

    Revert "fix: 49 - DEREFERENCE_NULLABLE - cb.onDataFetcherReady(sourceKey, data, loadData.fetcher, DataSource.DATA_DISK_CACHE, sourceKey); - dereferenced expression loadData is @Nullable"
    This reverts commit 52b625784ce79ab7bcada502697db2fc292f7209.

--- library/src/main/java/com/bumptech/glide/load/engine/DataCacheGenerator.java
+++ library/src/main/java/com/bumptech/glide/load/engine/DataCacheGenerator.java
@@ -99,12 +99,9 @@ class DataCacheGenerator implements DataFetcherGenerator, DataFetcher.DataCallba
   }
 
   @Override
-    public void onDataReady(@Nullable Object data) {
-      LoadData<?> local = loadData;
-      if (local != null) {
-        cb.onDataFetcherReady(sourceKey, data, local.fetcher, DataSource.DATA_DISK_CACHE, sourceKey);
-      }
-    }
+  public void onDataReady(@Nullable Object data) {
+    cb.onDataFetcherReady(sourceKey, data, loadData.fetcher, DataSource.DATA_DISK_CACHE, sourceKey);
+  }
 
   @Override
   public void onLoadFailed(@NonNull Exception e) {
