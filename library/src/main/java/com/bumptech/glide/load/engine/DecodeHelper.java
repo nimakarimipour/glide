c2be6a6eee982f1620d8b548cadf3235092a7d83
Nima Karimipour <karimipour.nima@gmail.com>
2025-07-13 09:39:06 -0700

    before:58to56 - Type='ASSIGN_FIELD_NULLABLE', message='assigning @Nullable expression to @NonNull field' /home/nima/Developer/nullness-benchmarks/glide/library/src/main/java/com/bumptech/glide/load/engine/DecodeHelper.java:85     resourceClass = null;

--- library/src/main/java/com/bumptech/glide/load/engine/DecodeHelper.java
+++ library/src/main/java/com/bumptech/glide/load/engine/DecodeHelper.java
@@ -79,21 +79,21 @@ final class DecodeHelper<Transcode> {
   }
 
   void clear() {
-      glideContext = null;
-      model = null;
-      signature = null;
-      resourceClass = null;
-      transcodeClass = null;
-      // options = null; // Removed assignment to null
-      priority = null;
-      transformations = null;
-      diskCacheStrategy = null;
-  
-      loadData.clear();
-      isLoadDataSet = false;
-      cacheKeys.clear();
-      isCacheKeysSet = false;
-    }
+        glideContext = null;
+        model = null;
+        signature = null;
+        // resourceClass = null; // Removed assignment to null
+        // transcodeClass = null; // Removed assignment to null
+        // options = null; // Removed assignment to null
+        priority = null;
+        transformations = null;
+        diskCacheStrategy = null;
+    
+        loadData.clear();
+        isLoadDataSet = false;
+        cacheKeys.clear();
+        isCacheKeysSet = false;
+      }
 
   DiskCache getDiskCache() {
     return diskCacheProvider.getDiskCache();
